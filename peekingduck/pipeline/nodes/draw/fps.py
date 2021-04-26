cf156e11562fee9ff016299473d7befa1222921c
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-04-26 12:13:36 +0800

    Added node for calculating FPS

--- peekingduck/pipeline/nodes/draw/fps.py
+++ peekingduck/pipeline/nodes/draw/fps.py
@@ -14,26 +14,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import Dict
+from typing import Any, Dict
 from time import perf_counter
 
 from peekingduck.pipeline.nodes.node import AbstractNode
+from .utils.drawfunctions import draw_fps
 
 class Node(AbstractNode):
+    """ FPS node class that calculates the FPS and draw the FPS onto the image
+    frame
+    """
     def __init__(self, config: Dict) -> None:
         super().__init__(config, node_path=__name__)
 
         self.current_frame_time = 0
         self.previous_frame_time = 0
 
-    def run(self, inputs: Dict):
+    def run(self, inputs: Dict[str, Any]) -> None:
+        """ Calculates FPS using the time difference between the current frame
+        and the previous frame. Calculated FPS is then draw onto image frame
+
+        Args:
+            inputs: ["img"]
+
+        Returns:
+            outputs: [None]
+        """
+
         self.current_frame_time = perf_counter()
-
         current_fps = 1 / (self.current_frame_time - self.previous_frame_time)
-
-        self.logger("FPS: " + current_fps)
-
         self.previous_frame_time = self.current_frame_time
+        draw_fps(inputs[self.inputs[0]], current_fps)
 
         return {}
 
