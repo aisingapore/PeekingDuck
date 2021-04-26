a21be89c6bd0c7ceb399c26787bb879f0de40522
pangyyyyy <pangyyyyyy96@gmail.com>
2021-04-26 20:06:43 +0800

    added scripts for posenet model

---
+++ peekingduck/pipeline/nodes/model/posenet.py
@@ -0,0 +1,41 @@
+"""
+Copyright 2021 AI Singapore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from typing import Dict
+from peekingduck.pipeline.nodes.node import AbstractNode
+from .posenetv1 import posenet_model
+
+
+class Node(AbstractNode):
+    """Node for PoseNet
+    """
+
+    def __init__(self, config):
+        super().__init__(config, node_path=__name__)
+        self.model = posenet_model.PoseNetModel(config)
+
+    def run(self, inputs: Dict):
+        """function that reads the image input and returns the bboxes
+        of the specified objects chosen to be detected
+
+        Args:
+            inputs (Dict): Dictionary of inputs
+
+        Returns:
+            outputs (Dict): poses output in dictionary format
+        """
+        results = self.model.predict(inputs[self.inputs[0]])
+        outputs = {self.outputs[0]: results}
+        return outputs
