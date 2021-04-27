11d8ef48486e1d5f3ad1f85af2ba40c206c9acad
leeping-ng <leeping_ng@hotmail.com>
2021-04-27 15:57:25 +0800

    updated naming conventions and changed from dict to list

--- peekingduck/pipeline/nodes/heuristic/check_nearby_objs.py
+++ peekingduck/pipeline/nodes/heuristic/check_nearby_objs.py
@@ -13,45 +13,40 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from typing import Dict, Any
 
 import numpy as np
-from typing import Dict
 
 from peekingduck.pipeline.nodes.node import AbstractNode
 
 
 class Node(AbstractNode):
-    def __init__(self, config: Dict) -> None:
-        super().__init__(config, name="heuristic.nearby_objects")
+    def __init__(self, config: Dict[str, Any]) -> None:
+        super().__init__(config, node_path=__name__)
         self.near_thres = config["near_threshold"]
         self.tag_msg = config["tag_msg"]
 
-    def run(self, inputs: Dict) -> Dict:
+    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """Compares the 3D locations of all objects to see which objects are close to each other.
         If an object is close to another, tag it.
 
         Args:
-            inputs: ["obj_3D_locs"]
+            inputs (dict): Dict with keys "obj_3D_locs".
 
         Returns:
-            outputs: ["obj_tags"]
+            outputs (dict): Dict with keys "obj_tags".
         """
 
-        objs_info = []
+        obj_tags = [""]*len(inputs["obj_3D_locs"])
 
-        for obj_1 in inputs["obj_3D_locs"]:
-            obj_info = {"idx": obj_1["idx"], "tag": " "}
-
+        for idx, obj_1 in enumerate(inputs["obj_3D_locs"]):
             for obj_2 in inputs["obj_3D_locs"]:
                 if obj_1 == obj_2:
                     continue
 
                 dist_bet = np.linalg.norm(obj_1["3D_loc"] - obj_1["3D_loc"])
                 if dist_bet < self.near_thres:
-                    obj_info["tag"] = self.tag_msg
+                    obj_tags[idx] = self.tag_msg
                     break
-            objs_info.append(obj_info)
 
-        outputs = {"obj_tags": objs_info}
-
-        return outputs
+        return {"obj_tags": obj_tags}
