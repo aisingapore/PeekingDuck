a70d9000907a3ab7a248e8782cbfe5204ada9aec
di_wei_tan <di_wei_tan@kelaberetiv.sg>
2021-04-27 11:50:00 +0800

    removed peoplecounting, standardized bbox to midpoint node

--- peekingduck/pipeline/nodes/heuristic/bbox_to_btm_midpoint.py
+++ peekingduck/pipeline/nodes/heuristic/bbox_to_btm_midpoint.py
@@ -14,36 +14,35 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import numpy as np
-from typing import Dict, List, Tuple
-
+from typing import Dict, Any, Tuple
 from peekingduck.pipeline.nodes.node import AbstractNode
 
 
 class Node(AbstractNode):
-    def __init__(self, config: Dict) -> None:
-        super().__init__(config, name='heuristic.bbox_to_btm_midpoint')
+    """This node converts bounding boxes to a singel pt which is
+    the bottom midpoint of the bounding box"""
 
-    def run(self, inputs: Dict) -> List[Tuple[float]]:
-        """Converts bounding boxes to  a single point of reference
+    def __init__(self, config: Dict) -> None:
+        super().__init__(config, node_path=__name__)
+        self.img_size = None
+
+    def run(self, inputs: Dict) -> Dict[str, Any]:
+        """Converts bounding boxes to a single point of reference
         for use in zone analytics
 
         Args:
-            bboxes (List[List[float]]): List of bboxes, each a list of 4 elements that
-            defines the bounding box 
-
+            inputs (dict): Dict with keys "bboxes" and "img".
         Returns:
-            bbox_pts (List[tuple[float]]): List of x, y coordinates of the mid lower pt
-            of each bounding box
+            outputs (dict): Dict with keys "btm_midpoint".
 
         """
         # get xy midpoint of each bbox (x1, y1, x2, y2)
         # This is calculated as x is (x1-x2)/2 and y is y2
-        bboxes = inputs[self.inputs[0]]
-        frame = inputs[self.inputs[1]]
+        bboxes = inputs["bboxes"]
+        frame = inputs["img"]
         self.img_size = (frame.shape[1], frame.shape[0])
         return {'btm_midpoint': [self._xy_on_img(((bbox[0]+bbox[2])/2), bbox[3])
-                for bbox in bboxes]}
+                                 for bbox in bboxes]}
 
-    def _xy_on_img(self, x, y):
-        return (int(x * self.img_size[0]), int(y * self.img_size[1]))
+    def _xy_on_img(self, pt_x: float, pt_y: float) -> Tuple[int]:
+        return (int(pt_x * self.img_size[0]), int(pt_y * self.img_size[1]))
