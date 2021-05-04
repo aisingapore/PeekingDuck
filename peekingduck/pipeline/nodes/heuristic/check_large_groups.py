06c84e0274d21ce4f0088210236998134f518057
leeping-ng <leeping_ng@hotmail.com>
2021-05-04 21:45:55 +0800

    changed return type from bool to int

--- peekingduck/pipeline/nodes/heuristic/check_large_groups.py
+++ peekingduck/pipeline/nodes/heuristic/check_large_groups.py
@@ -21,43 +21,25 @@ from peekingduck.pipeline.nodes.node import AbstractNode
 
 
 class Node(AbstractNode):
-    """This node checks which objects are in groups that have exceeded
-    the group size threshold."""
+    """This node checks which groups have exceeded the group size threshold."""
 
     def __init__(self, config: Dict[str, Any]) -> None:
         super().__init__(config, node_path=__name__)
         self.group_size_thres = config["group_size_thres"]
 
-    def run(self, inputs: Dict[str, List[int]]) -> Dict[str, List[bool]]:
-        """ Checks which objects are in groups that have exceeded the
-        group size threshold.
+    def run(self, inputs: Dict[str, List[int]]) -> Dict[str, List[int]]:
+        """ Checks which groups have exceeded the group size threshold,
+        and returns a list of such groups.
 
         Args:
             inputs (dict): Dict with keys "obj_groups".
 
         Returns:
-            outputs (dict): Dict with keys "is_obj_in_large_grp".
+            outputs (dict): Dict with keys "large_groups".
         """
 
-        is_obj_in_large_grp = []
-        large_groups = self._find_large_groups(
-            inputs["obj_groups"], self.group_size_thres)
-        for obj_group in inputs["obj_groups"]:
-            if obj_group in large_groups:
-                is_obj_in_large_grp.append(True)
-            else:
-                is_obj_in_large_grp.append(False)
-
-        return {"is_obj_in_large_grp": is_obj_in_large_grp}
-
-    @staticmethod
-    def _find_large_groups(group_alloc: List[int], group_size_thres: int) -> List[int]:
-        """ Creates a list containing the group numbers of groups that have exceeded
-        the size defined by group_size_thres.
-        """
-
-        group_counter = Counter(group_alloc)
+        group_counter = Counter(inputs["obj_groups"])
         large_groups = [group for group in group_counter if
-                        group_counter[group] > group_size_thres]
+                        group_counter[group] > self.group_size_thres]
 
-        return large_groups
+        return {"large_groups": large_groups}
