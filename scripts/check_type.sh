8f47b0efbdd27a88136008976c1059949c5d759e
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-05-05 18:49:02 +0800

    Update mypy

--- scripts/check_type.sh
+++ scripts/check_type.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! (mypy --ignore-missing-imports peekingduck); then
+if ! (mypy --ignore-missing-imports --strict --pretty --disable-error-code assignment  peekingduck); then
     echo "TYPE CHECK FAIL"
     exit 123
 else
