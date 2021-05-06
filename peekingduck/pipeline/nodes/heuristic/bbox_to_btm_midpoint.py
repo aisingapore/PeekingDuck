7e4a46ca92654d6962de33b57696be01db941d44
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-05-06 13:06:33 +0800

    Lint code

--- peekingduck/pipeline/nodes/heuristic/bbox_to_btm_midpoint.py
+++ peekingduck/pipeline/nodes/heuristic/bbox_to_btm_midpoint.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import Dict, Any, Tuple, Optional, List
+from typing import Dict, Any, Tuple, Optional
 from peekingduck.pipeline.nodes.node import AbstractNode
 
 
