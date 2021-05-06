1e9d438d100ada8e198327e6b7ceac386c1dfc90
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-05-06 11:53:25 +0800

    MYPY check only function annoation

--- scripts/check_type.sh
+++ scripts/check_type.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! (mypy --ignore-missing-imports --strict --disable-error-code assignment peekingduck); then
+if ! (mypy --ignore-missing-imports --disallow-untyped-defs peekingduck); then
     echo "TYPE CHECK FAIL"
     exit 123
 else
