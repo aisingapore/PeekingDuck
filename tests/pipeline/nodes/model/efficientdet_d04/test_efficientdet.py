b487874878ad31d546dcaa029e874c0927422404
NurmanZahin <nzahin_95@hotmail.com>
2021-05-24 12:48:54 +0800

    Removed unused arguments, added config fixture and added actual_scale tests

--- tests/pipeline/nodes/model/efficientdet_d04/test_efficientdet.py
+++ tests/pipeline/nodes/model/efficientdet_d04/test_efficientdet.py
@@ -23,28 +23,29 @@ from peekingduck.pipeline.nodes.model.efficientdet import Node
 from peekingduck.pipeline.nodes.model.efficientdet_d04.efficientdet_files.detector import Detector
 
 
-@pytest.fixture(params=[0, 1, 2, 3, 4])
-def efficientdet(request):
-    filepath = os.path.join(os.getcwd(), 'tests', 'pipeline', 'nodes',
-                            'model', 'efficientdet_d04', 'test_efficientdet.yml')
+@pytest.fixture
+def efficientdet_config():
+    filepath = os.path.join(
+        os.getcwd(), 'tests/pipeline/nodes/model/efficientdet_d04/test_efficientdet.yml')
     with open(filepath) as file:
-        node_config = yaml.load(file, Loader=yaml.FullLoader)
+        node_config = yaml.safe_load(file)
     node_config['root'] = os.getcwd()
-    node_config['model_type'] = request.param
-    node = Node(node_config)
+
+    return node_config
+
+
+@pytest.fixture(params=[0, 1, 2, 3, 4])
+def efficientdet(request, efficientdet_config):
+    efficientdet_config['model_type'] = request.param
+    node = Node(efficientdet_config)
 
     return node
 
 
-@pytest.fixture
-def efficientdet_detector(request):
-    filepath = os.path.join(os.getcwd(), 'tests', 'pipeline', 'nodes',
-                            'model', 'efficientdet_d04', 'test_efficientdet.yml')
-    with open(filepath) as file:
-        node_config = yaml.load(file, Loader=yaml.FullLoader)
-    node_config['root'] = os.getcwd()
-    node_config['model_type'] = 0
-    detector = Detector(node_config)
+@pytest.fixture()
+def efficientdet_detector(efficientdet_config):
+    efficientdet_config['model_type'] = 0
+    detector = Detector(efficientdet_config)
 
     return detector
 
@@ -66,7 +67,7 @@ class TestEfficientDet:
 
     def test_efficientdet_preprocess(self, create_image, efficientdet_detector):
         test_img1 = create_image((720, 1280, 3))
-        test_img2 = create_image((1280, 720, 3))
+        test_img2 = create_image((640, 480, 3))
         actual_img1, actual_scale1 = efficientdet_detector.preprocess(test_img1, 512)
         actual_img2, actual_scale2 = efficientdet_detector.preprocess(test_img2, 512)
 
@@ -74,6 +75,8 @@ class TestEfficientDet:
         assert actual_img2.shape == (512, 512, 3)
         assert actual_img1.dtype == np.float32
         assert actual_img2.dtype == np.float32
+        assert actual_scale1 == 0.4
+        assert actual_scale2 == 0.8
 
     def test_efficientdet_postprocess(self, efficientdet_detector):
         output_bbox = np.array([[1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0]])
