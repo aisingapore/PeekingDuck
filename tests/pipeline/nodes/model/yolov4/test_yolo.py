d9973380304ca0196a414ba2f4a97345ee15ad1a
tanddoubleu <rune0119@gmail.com>
2021-05-27 10:01:46 +0800

    added non detection and detection tests to yolo. Check coverage.

--- tests/pipeline/nodes/model/yolov4/test_yolo.py
+++ tests/pipeline/nodes/model/yolov4/test_yolo.py
@@ -19,6 +19,7 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 import cv2
+import tensorflow as tf
 from peekingduck.pipeline.nodes.model.yolo import Node
 from peekingduck.pipeline.nodes.model.yolov4.yolo_files.detector import Detector
 
@@ -50,50 +51,22 @@ def yolo_detector(yolo_config):
     return detector
 
 
-class TestEfficientDet:
+class TestYolo:
 
-    def test_no_human_image(self, test_no_human_images, efficientdet):
+    def test_no_human_image(self, test_no_human_images, yolo):
         blank_image = cv2.imread(test_no_human_images)
-        output = efficientdet.run({'img': blank_image})
-        expected_output = {'bboxes': np.empty((0, 4), dtype=np.float32)}
+        output = yolo.run({'img': blank_image})
+        expected_output = {'bboxes': np.empty((0, 4), dtype=np.float32),
+                           'bbox_labels': [],
+                           'bbox_scores': tf.zeros((0,))}
         assert output.keys() == expected_output.keys()
         npt.assert_equal(output['bboxes'], expected_output['bboxes'])
+        assert output['bbox_labels'] == expected_output['bbox_labels']
+        tf.debugging.assert_equal(output['bbox_scores'],
+                                  expected_output['bbox_scores'])
 
-    def test_return_at_least_one_person_and_one_bbox(self, test_human_images, efficientdet):
+    def test_return_at_least_one_person_and_one_bbox(self, test_human_images, yolo):
         test_img = cv2.imread(test_human_images)
-        output = efficientdet.run({'img': test_img})
+        output = yolo.run({'img': test_img})
         assert 'bboxes' in output
         assert output['bboxes'].size != 0
-
-    def test_efficientdet_preprocess(self, create_image, efficientdet_detector):
-        test_img1 = create_image((720, 1280, 3))
-        test_img2 = create_image((640, 480, 3))
-        actual_img1, actual_scale1 = efficientdet_detector.preprocess(test_img1, 512)
-        actual_img2, actual_scale2 = efficientdet_detector.preprocess(test_img2, 512)
-
-        assert actual_img1.shape == (512, 512, 3)
-        assert actual_img2.shape == (512, 512, 3)
-        assert actual_img1.dtype == np.float32
-        assert actual_img2.dtype == np.float32
-        assert actual_scale1 == 0.4
-        assert actual_scale2 == 0.8
-
-    def test_efficientdet_postprocess(self, efficientdet_detector):
-        output_bbox = np.array([[1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0]])
-        output_label = np.array([0, 0])
-        output_score = np.array([0.9, 0.2])
-        network_output = (output_bbox, output_score, output_label)
-        scale = 0.5
-        img_shape = (720, 1280)
-        detect_ids = [0]
-        boxes, labels, scores = efficientdet_detector.postprocess(
-            network_output, scale, img_shape, detect_ids)
-
-        expected_bbox = np.array([[1, 2, 3, 4]])/scale
-        expected_bbox[:, [0, 2]] /= img_shape[1]
-        expected_bbox[:, [1, 3]] /= img_shape[0]
-
-        expected_score = np.array([0.9])
-        npt.assert_almost_equal(expected_bbox, boxes)
-        npt.assert_almost_equal(expected_score, scores)
-        npt.assert_equal(np.array(['person']), labels)
