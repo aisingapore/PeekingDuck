96a2878c628e2f890559e6531f79ffb24565cea9
ongzizhao <61336482+ongzizhao@users.noreply.github.com>
2021-07-07 11:31:31 +0800

    feat(output.csv_writer): added doc strings to the private methods

--- peekingduck/pipeline/nodes/output/csv_writer.py
+++ peekingduck/pipeline/nodes/output/csv_writer.py
@@ -44,10 +44,20 @@ class Node(AbstractNode):
                 self._logging_interval)
 
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Write the current state of the tracked statistics into 
+        the csv file as a row entry
+
+        Args:
+            inputs(dict): the data pool of the pipeline
+
+        Returns:
+            outputs: [None]
+        """
 
         if not self._stats_checked:
             self._check_tracked_stats(inputs)
-            #self._stats_to_track might change after the check
+            # self._stats_to_track might change after the check
             self.csv_logger = CSVLogger(
                 self._filepath_datetime,
                 self._stats_to_track,
@@ -57,8 +67,11 @@ class Node(AbstractNode):
 
         return {}
 
-    def _check_tracked_stats(self,inputs: Dict[str, Any]) -> None:
-
+    def _check_tracked_stats(self, inputs: Dict[str, Any]) -> None:
+        """
+        Check whether user input statistics is present in the data pool of the pipeline
+        Statistics not present in data pool will be ignored and dropped
+        """
         valid=[]
         invalid=[]
 
@@ -73,9 +86,9 @@ class Node(AbstractNode):
                     {invalid} are not valid outputs.
                     Data pool only has this outputs: {list(inputs.keys())}
                     Only {valid} will be logged in the csv file""")
-            self.logger.info(msg)
+            self.logger.warning(msg)
 
-        #update _stats_to_track with valid stats found in data pool
+        # update _stats_to_track with valid stats found in data pool
         self._stats_to_track = valid
         self._stats_checked = True
 
@@ -87,12 +100,16 @@ class Node(AbstractNode):
 
     @staticmethod
     def _append_datetime_filepath(filepath: str) -> str:
+        """
+        Append time stamp to the filename 
+        """
         current_time = datetime.now() # type: ignore
-        time_str=current_time.strftime("%d%m%y-%H-%M-%S")  #output as '240621-15-09-13'
+        time_str = current_time.strftime("%d%m%y-%H-%M-%S")  # output as '240621-15-09-13'
 
-        #append timestamp to filename before extension Format: filename_timestamp.extension
-        filepath_with_timestamp = filepath.split(".")[-2] \
-            + "_" + time_str \
-            + "."+filepath.split(".")[-1]
+        file_name = filepath.split('.')[-2]
+        file_ext = filepath.split('.')[-1]
+
+        # append timestamp to filename before extension Format: filename_timestamp.extension
+        filepath_with_timestamp = f"{file_name}_{time_str}.{file_ext}"
 
         return filepath_with_timestamp
