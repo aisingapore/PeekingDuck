533df59f3b47a5575166c3b9fd52b9a204daf463
diwei-tan <rune0119@gmail.com>
2021-07-14 21:05:49 +0800

    feat(draw): converted brightness node to image processing node that handles both contrast and brightness

--- peekingduck/pipeline/nodes/draw/image_processor.py
+++ peekingduck/pipeline/nodes/draw/image_processor.py
@@ -17,31 +17,26 @@ Adjusts brightness of an incoming image
 
 
 from typing import Any, Dict
-from PIL import Image, ImageEnhance
-import numpy as np
 from peekingduck.pipeline.nodes.node import AbstractNode
+from peekingduck.pipeline.nodes.draw.utils.image_processor  \
+    import adjust_contrast_and_brightness
 
 
 class Node(AbstractNode):
-    """Node for changing image brightness"""
+    """Node for changing image contrast and brightness"""
 
     def __init__(self, config: Dict[str, Any]) -> None:
         super().__init__(config, node_path=__name__)
         self.brightness = config['brightness']
+        self.contrast = config ['contrast']
 
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """function that draws pose details onto input image
 
         Args:
             inputs (dict): Dict with keys "img"
-
-        Returns:
-            outputs (Dict): bbox output in dictionary format with keys
-            "img"
         """
-        img = Image.fromarray(inputs["img"])
-        enhancer = ImageEnhance.Brightness(img)
-        enhanced_img = enhancer.enhance(self.brightness)
-        inputs['img'] = np.array(enhanced_img)
-
-        return {"img": np.array(enhanced_img)}
+        img = adjust_contrast_and_brightness(inputs['img'],
+                                             self.contrast,
+                                             self.brightness)
+        return {"img": img}
