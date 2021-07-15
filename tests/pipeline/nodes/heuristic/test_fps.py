d485fc247e08ad2e723ee73a9f55857ddf3c75a1
Jovinder Singh <jovinder@yahoo.com>
2021-07-16 01:17:52 +0800

    fix(refactor): Add module docstring and changed moving_avg to False

--- tests/pipeline/nodes/heuristic/test_fps.py
+++ tests/pipeline/nodes/heuristic/test_fps.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2021 AI Singapore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import pytest
 from peekingduck.pipeline.nodes.heuristic.fps import Node
 
@@ -6,7 +21,7 @@ from peekingduck.pipeline.nodes.heuristic.fps import Node
 def fps_node():
     node = Node({"input": ["pipeline_end"],
                  "output": ["fps"],
-                 "moving_avg": True
+                 "moving_avg": False
                  })
     return node
 
