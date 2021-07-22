23d342b4abd0f2cd8732cb284cd5fb949cfe4cdb
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-07-22 10:56:04 +0800

    docs (nodes): refactor doc strings for efficientdet node

--- peekingduck/pipeline/nodes/model/efficientdet.py
+++ peekingduck/pipeline/nodes/model/efficientdet.py
@@ -23,11 +23,42 @@ from peekingduck.pipeline.nodes.model.efficientdet_d04 import efficientdet_model
 
 
 class Node(AbstractNode):
-    """EfficientDet node class that initializes and uses efficientdet model to detect
-    bounding boxes from an image.
+    """EfficientDet node that initalises a EfficientDet model to detect bounding 
+    boxes from an image.
 
-    EfficientDet: Scalable and Efficient Object Detection
-    https://arxiv.org/abs/1911.09070
+    The EfficientDet node is capable of detecting objects from 80 categories. The 
+    table of categories can be found :term:`here <object detection indices>`.  
+    EfficientDet node has five levels of compound coefficient (0 - 5). The 
+    default compound coefficient is 0 and can be changed to other values. 
+    
+    Inputs:
+        |img|
+
+    Outputs:
+        |bboxes|
+
+        |bbox_labels|
+
+        |bbox_scores|
+
+    Configs:
+        model_type (:obj:`int`): **{"0", "1", "2", "3", "4"}, default="0"**
+
+            Defines the compound coefficient for EfficientDet.
+
+        score_threshold (:obj:`float`): **[0,1], default = 0.3**
+
+            Threshold to determine if detection should be returned
+
+        detect_ids (:obj:`List`): **default = [0] **
+
+            List of object class ids to be detected.
+
+    References:
+
+    EfficientDet: Scalable and Efficient Object Detection: https://arxiv.org/abs/1911.09070
+
+    Code adapted from https://github.com/xuannianz/EfficientDet
     """
 
     def __init__(self, config: Dict[str, Any]) -> None:
@@ -37,12 +68,6 @@ class Node(AbstractNode):
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """Function that takes an image as input and returns bboxes of objects specified
         in config.
-
-        Args:
-            inputs (Dict): Dict with key "img"
-
-        Returns:
-            outputs (Dict): Dict with keys "bboxes".
         """
         # Currently prototyped to return just the bounding boxes
         # without the scores
