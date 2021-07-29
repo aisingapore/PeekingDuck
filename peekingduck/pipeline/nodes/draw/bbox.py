a3fd8cd3223e074d04c3b7a2f3c8cbd1c3be210b
diwei-tan <rune0119@gmail.com>
2021-07-29 22:15:28 +0800

    feat(draw): use different colours for different types of object when multiple objects are chosen for object detection

--- peekingduck/pipeline/nodes/draw/bbox.py
+++ peekingduck/pipeline/nodes/draw/bbox.py
@@ -29,9 +29,6 @@ class Node(AbstractNode):
         self.show_labels = config['show_labels']
 
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        if self.show_labels:
-            draw_bboxes(inputs["img"], inputs["bboxes"],
-                        CHAMPAGNE, inputs["bbox_labels"])  # type: ignore
-        else:
-            draw_bboxes(inputs["img"], inputs["bboxes"], CHAMPAGNE)  # type: ignore
+        draw_bboxes(inputs["img"], inputs["bboxes"],
+                    inputs["bbox_labels"], self.show_labels)  # type: ignore
         return {}
