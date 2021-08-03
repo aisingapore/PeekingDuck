c78e650158035ccd4405153c2650db76a70facc6
ongzizhao <61336482+ongzizhao@users.noreply.github.com>
2021-08-03 13:33:30 +0800

    left 2 images

--- tests/pipeline/nodes/model/yolov4/test_yolo.py
+++ tests/pipeline/nodes/model/yolov4/test_yolo.py
@@ -60,16 +60,16 @@ def replace_download_weights(root, blob_file):
 @pytest.mark.mlmodel
 class TestYolo:
 
-    # def test_no_human_image(self, test_no_human_images, yolo):
-    #     blank_image = cv2.imread(test_no_human_images)
-    #     output = yolo.run({'img': blank_image})
-    #     expected_output = {'bboxes': np.empty((0, 4), dtype=np.float32),
-    #                        'bbox_labels': np.empty((0)),
-    #                        'bbox_scores': np.empty((0), dtype=np.float32)}
-    #     assert output.keys() == expected_output.keys()
-    #     npt.assert_equal(output['bboxes'], expected_output['bboxes'])
-    #     npt.assert_equal(output['bbox_labels'], expected_output['bbox_labels'])
-    #     npt.assert_equal(output['bbox_scores'], expected_output['bbox_scores'])        
+    def test_no_human_image(self, test_no_human_images, yolo):
+        blank_image = cv2.imread(test_no_human_images)
+        output = yolo.run({'img': blank_image})
+        expected_output = {'bboxes': np.empty((0, 4), dtype=np.float32),
+                           'bbox_labels': np.empty((0)),
+                           'bbox_scores': np.empty((0), dtype=np.float32)}
+        assert output.keys() == expected_output.keys()
+        npt.assert_equal(output['bboxes'], expected_output['bboxes'])
+        npt.assert_equal(output['bbox_labels'], expected_output['bbox_labels'])
+        npt.assert_equal(output['bbox_scores'], expected_output['bbox_scores'])        
 
     def test_return_at_least_one_person_and_one_bbox(self, test_human_images, yolo):
         logger.warning(f"i reached here {test_human_images}")
@@ -79,21 +79,21 @@ class TestYolo:
         assert 'bboxes' in output
         assert output['bboxes'].size != 0
 
-    # def test_no_weights(self, yolo_config):
-    #     with mock.patch('peekingduck.weights_utils.checker.has_weights',
-    #                     return_value=False):
-    #         with mock.patch('peekingduck.weights_utils.downloader.download_weights',
-    #                         wraps=replace_download_weights):
-    #             with TestCase.assertLogs(
-    #                 'peekingduck.pipeline.nodes.model.yolov4.yolo_model.logger') \
-    #                 as captured:
+    def test_no_weights(self, yolo_config):
+        with mock.patch('peekingduck.weights_utils.checker.has_weights',
+                        return_value=False):
+            with mock.patch('peekingduck.weights_utils.downloader.download_weights',
+                            wraps=replace_download_weights):
+                with TestCase.assertLogs(
+                    'peekingduck.pipeline.nodes.model.yolov4.yolo_model.logger') \
+                    as captured:
 
-    #                 yolo = Node(yolo_config)
+                    yolo = Node(yolo_config)
 
-    #                 assert captured.records[0].getMessage(
-    #                 ) == '---no yolo weights detected. proceeding to download...---'
-    #                 assert captured.records[1].getMessage(
-    #                 ) == '---yolo weights download complete.---'
+                    assert captured.records[0].getMessage(
+                    ) == '---no yolo weights detected. proceeding to download...---'
+                    assert captured.records[1].getMessage(
+                    ) == '---yolo weights download complete.---'
 
-    # def test_get_detect_ids(self, yolo):
-    #     assert yolo.model.get_detect_ids() == [0] 
+    def test_get_detect_ids(self, yolo):
+        assert yolo.model.get_detect_ids() == [0] 
