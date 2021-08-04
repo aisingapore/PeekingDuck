159b9d405362ebb921f33be64072b4a0282555b7
Jovinder Singh <jovinder@yahoo.com>
2021-08-04 18:45:49 +0800

    refactor(cli): check extension within list

--- peekingduck/pipeline/nodes/input/live.py
+++ peekingduck/pipeline/nodes/input/live.py
@@ -70,7 +70,7 @@ class Node(AbstractNode):
 
     def __init__(self, config: Dict[str, Any]=None, **kwargs: Any) -> None:
         super().__init__(config, node_path=__name__, **kwargs)
-
+        self._allowed_extensions = ["mp4", "avi", "mov", "mkv"]
         self.videocap = VideoThread(self.input_source, self.mirror_image)
 
         width, height = self.videocap.resolution
@@ -79,8 +79,9 @@ class Node(AbstractNode):
             self.logger.info('Resizing of input set to %s by %s',
                              self.resize['width'],
                              self.resize['height'])
-        if not any(ext in config['filename'] for ext in ['.mov', '.avi', '.mp4']):
-            raise ValueError("filename extension must be one of: .avi, .mov, .mp4")
+        if self.filename.split('.')[-1] not in self._allowed_extensions:
+            raise ValueError("filename extension must be one of: %s", \
+                             self._allowed_extensions)
 
         self.frame_counter = 0
 
