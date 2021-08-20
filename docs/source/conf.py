e77df5d26dd46ba90ef870995a7867531d3b2871
sidney-tio <sidney.tioxr@gmail.com>
2021-08-20 16:58:29 +0800

    docs (conf.py): Added new params for new RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,15 +61,19 @@ source_suffix = ['.rst', '.md']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
-html_logo = "https://raw.githubusercontent.com/aimakerspace/PeekingDuck/dev/images/readme/peekingduck.png"
+html_style = 'css/pkdk.css'
+html_logo = "../../images/readme/peekingduck.png"
 html_sidebars = {
     '**': ['globaltoc.html', 'searchbox.html']
+}
+html_theme_options = {
+    'logo_only': True
 }
 
 autosummary_mock_imports = [
