e89242e875e63f1c01138f8c907eda4ff614a7f7
Ziyang Kevin Chng <chngzyk@gmail.com>
2021-09-06 22:29:01 +0800

    (logger): Change code implementation to allow color logging in Jupyter Notebook

--- peekingduck/utils/logger.py
+++ peekingduck/utils/logger.py
@@ -16,29 +16,55 @@
 Universal logging configueration
 """
 
+import sys
 import logging
+from typing import Optional, Dict
 
-import coloredlogs
-
-LEVEL_COLOR = {'info': {'bold': True, 'color': 'green'},
-               'warning': {'bold': True, 'color': 'yellow'},
-               'error': {'bold': True, 'color': 'red'},
-               'critical': {'bold': True, 'color': 'red'}}
-
-DEFAULT_FIELD_STYLES = {'asctime': {'color': 'green'},
-                        'hostname': {'color': 'yellow'},
-                        'levelname': {'color': 'blue', 'bold': True},
-                        'name': {'color': 'magenta'}}
+from colorama import Fore, Style
 
 
 def setup_logger() -> None:
-    """
-    Universal logging configuration
-    """
+    """ Universal logging configuration """
 
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(name)s %(levelname)s:%(message)s',
-                        datefmt="%Y-%m-%dT%H:%M:%S")
+    msg_formatt = '{asctime} {name} {level_color} {levelname} {reset}: {msg_color} {message} {reset}'
 
-    coloredlogs.install(level_styles=LEVEL_COLOR,
-                        field_styles=DEFAULT_FIELD_STYLES)
+    formatter = ColoredFormatter(msg_formatt, style='{', datefmt='%Y-%m-%d %H:%M:%S',
+                                 colors={'DEBUG': Fore.CYAN + Style.BRIGHT,
+                                         'INFO': Fore.GREEN + Style.BRIGHT,
+                                         'WARNING': Fore.YELLOW + Style.BRIGHT,
+                                         'ERROR': Fore.RED + Style.BRIGHT,
+                                         'CRITICAL': Fore.RED + Style.BRIGHT})
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    logger.handlers[:] = []
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    logger.info("Information!!!!")
+    logger.debug("Debug!!!!")
+    logger.warning("Warning!!!!")
+    logger.error("Error!!!!")
+    logger.critical("Critical!!!!")
+
+
+class ColoredFormatter(logging.Formatter):
+    """This class formatt the color of logging messages"""
+
+    def __init__(self, *args, colors: Optional[Dict[str, str]] = None, **kwargs) -> None:
+        """Initialize the formatter with specified format strings"""
+
+        super().__init__(*args, **kwargs)
+
+        self.colors = colors if colors else {}
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the specified record as text."""
+
+        record.level_color = self.colors.get(record.levelname, '')
+        record.reset = Style.RESET_ALL
+        record.msg_color = Fore.BLUE + Style.BRIGHT
+
+        return super().format(record)
