d7567922d9a39e95bcc5ad95868c9960ba2c0de7
Jovinder Singh <jovinder@yahoo.com>
2021-10-13 22:19:43 +0800

    feat(heuristics): script for iou tracker

---
+++ peekingduck/pipeline/nodes/dabble/utils/tracking_files/iou_tracker/iou_tracker.py
@@ -0,0 +1,88 @@
+# Original copyright (c) 2017 TU Berlin, Communication Systems Group
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so.
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Core IOU Tracker
+"""
+
+from .misc import iou_xywh as iou
+from .tracker import Tracker
+
+
+class IOUTracker(Tracker):
+    """
+    Intersection over Union Tracker.
+
+    References
+    ----------
+    * Implementation of this algorithm is heavily based on:
+    https://github.com/bochinski/iou-tracker
+
+    Args:
+        max_lost (int): Maximum number of consecutive frames object was not detected.
+        tracker_output_format (str): Output format of the tracker.
+        min_detection_confidence (float): Threshold for minimum detection confidence.
+        max_detection_confidence (float): Threshold for max. detection confidence.
+        iou_threshold (float): Intersection over union minimum value.
+    """
+
+    def __init__(
+            self,
+            max_lost=2,
+            iou_threshold=0.5,
+            min_detection_confidence=0.4,
+            max_detection_confidence=0.7,
+            tracker_output_format='mot_challenge'
+    ):
+        self.iou_threshold = iou_threshold
+        self.max_detection_confidence = max_detection_confidence
+        self.min_detection_confidence = min_detection_confidence
+
+        super(IOUTracker, self).__init__(max_lost=max_lost,
+            tracker_output_format=tracker_output_format)
+
+    def update(self, bboxes, detection_scores, class_ids):
+        detections = Tracker.preprocess_input(bboxes, class_ids, detection_scores)
+        self.frame_count += 1
+        track_ids = list(self.tracks.keys())
+
+        updated_tracks = []
+        for track_id in track_ids:
+            if len(detections) > 0:
+                idx, best_match = max(enumerate(detections), key=lambda x: \
+                                        iou(self.tracks[track_id].bbox, x[1][0]))
+                (bb, cid, scr) = best_match
+
+                if iou(self.tracks[track_id].bbox, bb) > self.iou_threshold:
+                    self._update_track(track_id, self.frame_count, bb, scr, class_id=cid,
+                                       iou_score=iou(self.tracks[track_id].bbox, bb))
+                    updated_tracks.append(track_id)
+                    del detections[idx]
+
+            if len(updated_tracks) == 0 or track_id is not updated_tracks[-1]:
+                self.tracks[track_id].lost += 1
+                if self.tracks[track_id].lost > self.max_lost:
+                    self._remove_track(track_id)
+
+        for bb, cid, scr in detections:
+            self._add_track(self.frame_count, bb, scr, class_id=cid)
+
+        outputs = self._get_tracks(self.tracks)
+        return outputs
---
+++ peekingduck/pipeline/nodes/dabble/utils/tracking_files/iou_tracking.py
@@ -0,0 +1,83 @@
+# Copyright 2021 AI Singapore
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tracking algorithm that uses IOU matching
+"""
+
+from typing import Any, Dict, List
+import numpy as np
+from .iou_tracker.iou_tracker import IOUTracker
+from .iou_tracker.utils import format_boxes
+
+
+class IOUTracking:
+    """Simple tracking class based on Intersection Over Union of bounding
+    boxes.
+
+    This method is based on the assumption that the detector produces a
+    detection per frame for every object to be tracked. Furthermore, it
+    is assumed that detections of an object in consecutive frames have
+    an unmistakably high overlap IOU (intersection-over-union) which is
+    commonly the case when using sufficiently high frame rates.
+
+    References:
+
+    High-Speed Tracking-by-Detection Without Using Image Information:
+        http://elvera.nue.tu-berlin.de/files/1517Bochinski2017.pdf
+
+    Inference code adapted from https://github.com/adipandas/multi-object-tracker
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.tracker = IOUTracker(max_lost=10,
+                            iou_threshold=0.1,
+                            min_detection_confidence=0.2,
+                            max_detection_confidence=1)
+
+    def run(self, inputs: Dict[str, Any]) -> List[int]:
+        """Update tracker on each frame and return sorted object tags"""
+        frame = np.copy(inputs["img"])
+        original_h, original_w, _ = frame.shape
+        bboxes = np.copy(inputs["bboxes"])
+        # Format bboxes from normalized to frame axis
+        bboxes = format_boxes(bboxes, original_h, original_w)
+        confidences = np.copy(inputs["bbox_scores"])
+        class_ids = self._convert_class_label_to_unique_id( \
+            np.copy(inputs["bbox_labels"]))
+
+        # Update trackers with current bboxes and scores
+        tracks = self.tracker.update(bboxes, confidences, class_ids)
+        # Order object tags by current bbox order
+        obj_tags = self._order_tags_by_bbox(bboxes, tracks)
+        return obj_tags
+
+    def _order_tags_by_bbox(self, bboxes: List[float], tracks: List[float]) -> List[str]:
+        """Order object tags by bboxes"""
+        # Create dict with {(bbox): id}
+        matching_dict = dict()
+        for trk in tracks:
+            matching_dict.update({(trk[2], trk[3], trk[4], trk[5]): trk[1]})
+        obj_tags = []
+        # Match current bbox with dict key and return id (value)
+        for bbox in bboxes:
+            obj_tag = matching_dict[tuple(bbox)]
+            obj_tags.append(str(obj_tag))
+        return obj_tags
+
+    def _convert_class_label_to_unique_id(self, classes: List[str]) -> List[int]:
+        """Covert class label to unique ids"""
+        id = dict([(y,x+1) for x,y in enumerate(sorted(set(classes)))])
+        class_ids = [id[x] for x in classes]
+        return class_ids
