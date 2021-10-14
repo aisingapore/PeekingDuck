46ab90dd29189e3e15e34f453570fc6968055321
yier <liyier90@gmail.com>
2021-10-14 09:21:00 +0800

    added black formatter check

---
+++ scripts/check_format.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eu
+
+fail = "0"
+
+black --version
+
+if ! black --check .; then
+    fail="1"
+fi
+
+if [ "$fail" = "1" ]; then
+    echo "At least one file is poorly formatted - check the output above"
+    exit 123
+fi
+
+echo "Everything seems to be formatted properly!"
