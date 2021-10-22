b33d648a0196f68b5cb0580c6e56298b36161472
yier <liyier90@gmail.com>
2021-10-22 17:56:52 +0800

    added documentation and type hints for lint checks

--- peekingduck/utils/requirement_checker.py
+++ peekingduck/utils/requirement_checker.py
@@ -1,15 +1,44 @@
+# Copyright 2021 AI Singapore
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Python package requirements checker
+"""
+
 import logging
 from pathlib import Path
 import subprocess
+from typing import Iterator, TextIO, Union
 
 import pkg_resources as pkg
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 ROOT = Path(__file__).resolve().parents[2]
 
-def check_requirements(
-    identifier, requirements_path=ROOT / "requirements.txt", install=True
-):
+def check_requirements(identifier: str,
+                       requirements_path: Path = ROOT / "requirements.txt",
+                       install: bool = True) -> None:
+    """Checks if the packages specified by the `identifier` in the requirements
+    file at `requirements_path` are present on the system. If `install` is True,
+    attempts to install the packages
+
+    Args:
+        identifier (str): A unique identifier, typically a pipeline node name,
+            used to specify which packages to check for
+        requiremetns_path (Path): Path to the requirements file
+        install (bool): Flag to specify missing packages should be installed
+    """
     with open(requirements_path) as infile:
         requirements = [f"{req.name}{req.specifier}"
                         for req in parse_requirements(infile, identifier)]
@@ -26,26 +55,27 @@ def check_requirements(
                     # Put req in quotes to prevent >=ver from being intepreted
                     # as redirect
                     logger.info(subprocess.check_output(
-                        f"pip install '{req}'", shell=True).decode())
+                        ["pip", "install", req]).decode())
                     n_update += 1
-                except subprocess.CalledProcessError as e:
-                    logger.error(e)
+                except subprocess.CalledProcessError as exception:
+                    logger.error(exception)
                     raise
             else:
-                logger.warn("%s. Please install and rerun.", msg)
+                logger.warning("%s. Please install and rerun.", msg)
 
     if n_update > 0:
-        logger.warn("%d package%s updated. Please rerun for the updates to "
-                    "take effect.", n_update, "s" * int(n_update > 1))
+        logger.warning("%d package%s updated. Please rerun for the updates to "
+                       "take effect.", n_update, "s" * int(n_update > 1))
 
 
-def parse_requirements(strs, identifier):
-    """Yield `pkg.Requirement` objects for each specification in `strs`
+def parse_requirements(file: TextIO,
+                       identifier: str) -> Iterator[pkg.Requirement]:
+    """Yield `pkg.Requirement` objects for each specification in `strings`
 
-    `strs` must be a string, or a (possibly-nested) iterable thereof.
+    `strings` must be a string, or a (possibly-nested) iterable thereof.
     """
     # create a steppable iterator, so we can handle \-continuations
-    lines = iter(yield_lines(strs, identifier))
+    lines = iter(yield_lines(file, identifier))
 
     for line in lines:
         # Drop comments -- a hash without a space may be in a URL.
@@ -58,19 +88,19 @@ def parse_requirements(strs, identifier):
                 line += next(lines)
             except StopIteration:
                 return
-        yield pkg.Requirement(line)
+        yield pkg.Requirement(line)  # type: ignore
 
 
-def yield_lines(strs, identifier):
+def yield_lines(strings: Union[TextIO, str], identifier: str) -> Iterator[str]:
     """Yield non-empty/non-comment lines of a string or sequence"""
     prefix = f"# OTF_req {identifier} "
-    if isinstance(strs, str):
-        for s in strs.splitlines():
-            s = s.strip()
+    if isinstance(strings, str):
+        for string in strings.splitlines():
+            string = string.strip()
             # Return only OTF_req lines
-            if s and s.startswith(prefix):
-                yield s[len(prefix):]
+            if string and string.startswith(prefix):
+                yield string[len(prefix):]
     else:
-        for ss in strs:
-            for s in yield_lines(ss, identifier):
-                yield s
+        for string_item in strings:
+            for string in yield_lines(string_item, identifier):
+                yield string
