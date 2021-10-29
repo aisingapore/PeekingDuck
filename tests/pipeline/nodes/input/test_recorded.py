444c700b628471522c34b15b480a589a6b21b699
yier <liyier90@gmail.com>
2021-10-29 14:36:43 +0800

    replaced os.path with pathlib.Path in input.recorded related files

--- tests/pipeline/nodes/input/test_recorded.py
+++ tests/pipeline/nodes/input/test_recorded.py
@@ -14,10 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-
 import numpy as np
-
 import pytest
+
 from peekingduck.pipeline.nodes.input.recorded import Node
 
 
