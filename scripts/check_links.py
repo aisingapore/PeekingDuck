9c7ee2442c79a51560b5824151faad781dda9197
yier <liyier90@gmail.com>
2021-10-30 20:49:29 +0800

    replaced os.path with pathlib.Path in check_links script and refactored links filtering logic

--- scripts/check_links.py
+++ scripts/check_links.py
@@ -1,7 +1,11 @@
-import os
+"""Checks for faulty links in documentation"""
+
 import urllib.request
-from bs4 import BeautifulSoup
+from pathlib import Path
+from typing import Iterator, List, Tuple, Union
+
 import markdown
+from bs4 import BeautifulSoup
 from texttable import Texttable
 
 
@@ -9,102 +13,123 @@ from texttable import Texttable
 # do not show in the Peekingduck repo on github
 #
 # def get_html():
-#     lst_html = []
 #     # search path is hard coded with ref of this script locaiton
-#     for root, dirs, files in os.walk(
-#         os.path.join(".", "docs", "build", "html"), topdown=False
-#     ):
-#         for name in files:
-#             if name[-4:] == "html":
-#                 filepath = os.path.join(root, name)
-#                 lst_html.append((filepath, root))
-#     return lst_html
+#     return [
+#         path
+#         for path in walk(Path.cwd() / "docs" / "build" / "html")
+#         if path.suffix == ".html"
+#     ]
 
 
-def get_md_rst():
-    lst_md_rst = []
-    # search path is hard coded with ref to github actions
-    # will ref to Peekingduck root folder
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            if name[-2:] == "md" or name[-3:] == "rst":
-                filepath = os.path.join(root, name)
-                lst_md_rst.append((filepath, root))
-    return lst_md_rst
+def walk(top: Path) -> Iterator[Path]:
+    """Walks a given directory tree using pathlib.Path.
+
+    Returns:
+        (Iterator): An iterator of the file paths in the directory tree
+    """
+    for path in top.iterdir():
+        if path.is_dir():
+            yield from walk(path)
+            continue
+        yield path.resolve()
 
 
-def check_files(lst_filepaths):
-    faulty_links = []
-    for filepath, root in lst_filepaths:
+def get_md_rst_paths() -> List[Path]:
+    """Returns all .md and .rst files in the repository"""
+    return [path for path in walk(Path.cwd()) if path.suffix in (".md", ".rst")]
 
-        with open(filepath, "r", encoding="utf-8") as f:
-            file = f.read()
-            file_as_html = markdown.markdown(file)
-            soup = BeautifulSoup(file_as_html, "html.parser")
-            filtered_by_img_tags = soup.find_all("img")
-            filtered_by_href_tags = soup.find_all("a")
+
+def check_for_faulty_links(
+    file_paths: List[Path],
+) -> List[Tuple[Path, str, Union[int, Path]]]:
+    """Returns faulty links from documentation files in the repository.
+
+    Parse the provided .md and .rst files for faulty hyperlinks or faulty
+    relative path links. For URLs, the current implementation only returns links
+    which give HTTP 404.
+
+    Args:
+        file_paths (List[Path]): File paths of all .md and .rst files in the
+            repository.
+
+    Returns:
+        (List[Tuple[Path, str, Union[int, Path]]]): A list of file paths in
+            which faulty links are found, the corresponding faulty links, and
+            the root folder/request error code.
+    """
+    faulty_links: List[Tuple[Path, str, Union[int, Path]]] = []
+    for path in file_paths:
+        with open(path, "r", encoding="utf-8") as infile:
+            content = infile.read()
+            content_html = markdown.markdown(content)
+            soup = BeautifulSoup(content_html, "html.parser")
             img_links = [
-                tag["src"] for tag in filtered_by_img_tags if tag.has_attr("src")
+                tag["src"]
+                for tag in soup.find_all(
+                    lambda tag: tag.name == "img" and tag.get("src")
+                )
             ]
             href_links = [
-                tag["href"] for tag in filtered_by_href_tags if tag.has_attr("href")
+                tag["href"]
+                for tag in soup.find_all(
+                    lambda tag: tag.name == "a" and tag.get("href")
+                )
             ]
-            total_list_of_links = img_links + href_links
-            final_list_of_links = [txt for txt in total_list_of_links if ("." in txt)]
-            # to extract the valid link from some links such as ./io_draw_nodes.md#input-nodes
-            final_list_of_links = [link.split("#")[0] for link in final_list_of_links]
-
-        for link in final_list_of_links:
-            # if link is a https link, run request.urlopen
-            if link[:4] == "http":
+            # "." filters out section links, split("#")[0] to filter out URI
+            # fragments
+            all_links = [
+                link.split("#")[0]
+                for link in filter(lambda link: "." in link, img_links + href_links)
+            ]
+        for link in all_links:
+            if link.startswith("http"):
                 try:
-                    resp = urllib.request.urlopen(link)  # nosec
-                except Exception as e:
+                    # Validated the URL to start with "http"
+                    urllib.request.urlopen(link)  # nosec
+                except urllib.error.HTTPError as error:
                     # In this implementation only 404 is flagged for broken links
                     # 404 = http page not found error
-                    # if statement can be removed/adjusted to flag multiple error codes
-                    # such as 404,403,408...
-                    if e.code == 404:
-                        # filepath is the current file being parsed
+                    # if statement can be removed/adjusted to flag multiple error
+                    # codes such as 404,403,408...
+                    if error.code == 404:
+                        # path is the current file being parsed
                         # link is the link found in the current parsed file
                         # e.code is the execption code
-                        faulty_links.append([filepath, link, e.code])
-
+                        rel_path = path.relative_to(Path.cwd())
+                        faulty_links.append((rel_path, link, error.code))
             else:
-                check = os.path.exists(os.path.join(root, link))
-
-                if not check:
-                    # filepath is the current file being parsed
+                if not (path.parent / link).exists():
+                    # path is the current file being parsed
                     # link is the link found in the current parsed file
                     # root is the root folder of the filepath of current file
                     condition = ["/peekingduck", "pipeline", "nodes"]
-                    if link.split(".")[0:3] == condition:
-                        pass
-
-                    else:
-                        faulty_links.append([filepath, link, root])
-
-        print(f"Checked {filepath}")
-
+                    if link.split(".")[:3] != condition:
+                        rel_path = path.relative_to(Path.cwd())
+                        faulty_links.append((rel_path, link, rel_path.parent))
+        print(f"Checked {path}")
     return faulty_links
 
 
-def print_output(faulty_links):
+def print_output(faulty_links: List[Tuple[Path, str, Union[int, Path]]]) -> None:
+    """Displays the list of file paths and faulty links in a table
 
+    Args:
+        faulty_links (List[Tuple[Path, str, Union[int, Path]]]): A list of file
+            paths in which faulty links are found, the corresponding faulty
+            links, and the root folder/request error code.
+    """
     print("\nTable of broken links\n")
-    t = Texttable()
-    t.set_cols_width([25, 25, 20])
-    t.add_rows(
-        [["Filepath", "Broken_Link", "Root_Folder / Request Error Code"]] + faulty_links
-    )
-    print(t.draw())
+    table = Texttable()
+    table.set_cols_width([25, 25, 20])
+    table.header(("Filepath", "Broken_Link", "Root_Folder / Request Error Code"))
+    table.add_rows(faulty_links, False)
+    print(table.draw())
 
 
 if __name__ == "__main__":
-
-    mds_rst_filepaths = get_md_rst()
+    MD_RST_PATHS = get_md_rst_paths()
     print("\nCHECKING FILES")
     print("-" * 50)
-    faulty_links = check_files(mds_rst_filepaths)
 
-    print_output(faulty_links)
+    FAULTY_LINKS = check_for_faulty_links(sorted(MD_RST_PATHS)[:10])
+    print_output(FAULTY_LINKS)
