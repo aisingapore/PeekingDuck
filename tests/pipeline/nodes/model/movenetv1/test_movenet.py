0507ac572927edd83560b7e813f5048b1d7d55c8
ongzizhao <61336482+ongzizhao@users.noreply.github.com>
2021-11-16 22:22:35 +0800

    feat(movenet.model): uploaded the unit tests for the node

---
+++ tests/pipeline/nodes/model/movenetv1/test_movenet.py
@@ -0,0 +1,206 @@
+"""
+Copyright 2021 AI Singapore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import pytest
+import yaml
+import cv2
+import os
+import numpy as np
+import numpy.testing as npt
+from unittest import mock, TestCase
+from peekingduck.pipeline.nodes.model.movenet import Node
+from peekingduck.pipeline.nodes.model.movenetv1 import movenet_model
+
+TEST_DIR = os.path.join(os.getcwd(), "images", "testing")
+single_model = ["singlepose_lightning", "singlepose_thunder", "multipose_lightning"]
+multi_model = ["multipose_lightning"]
+single_person_list = ["t2.jpg"]
+multi_person_list = ["t1.jpg", "t4.jpg"]
+empty_image_list = ["black.jpg", "t3.jpg"]
+invalid_thresholds = [-1, 1.1]
+
+
+@pytest.fixture(params=empty_image_list)
+def empty_image(request):
+    yield request.param
+
+
+@pytest.fixture(params=single_person_list)
+def single_person_image(request):
+    yield request.param
+
+
+@pytest.fixture(params=multi_person_list)
+def multi_person_image(request):
+    yield request.param
+
+
+@pytest.fixture(params=invalid_thresholds)
+def invalid_thresholds(request):
+    yield request.param
+
+
+@pytest.fixture
+def movenet_config():
+    filepath = os.path.join(
+        os.getcwd(), "tests/pipeline/nodes/model/movenetv1/test_movenet.yml",
+    )
+    with open(filepath) as file:
+        node_config = yaml.safe_load(file)
+    node_config["root"] = os.getcwd()
+
+    return node_config
+
+
+@pytest.fixture()
+def model(movenet_config):
+    model = movenet_model(movenet_config)
+    return model
+
+
+@pytest.fixture(params=single_model)
+def movenet_model_single(request, movenet_config):
+    movenet_config["model_type"] = request.param
+    node = Node(movenet_config)
+    return node
+
+
+@pytest.fixture(params=multi_model)
+def movenet_model_multi(request, movenet_config):
+    movenet_config["model_type"] = request.param
+    node = Node(movenet_config)
+    return node
+
+
+def replace_download_weights(root, blob_file):
+    return False
+
+
+@pytest.mark.mlmodel
+class TestMoveNet:
+    def test_no_weights(self, movenet_config):
+        with mock.patch(
+            "peekingduck.weights_utils.checker.has_weights", return_value=False
+        ):
+            with mock.patch(
+                "peekingduck.weights_utils.downloader.download_weights",
+                wraps=replace_download_weights,
+            ):
+                with TestCase.assertLogs(
+                    "peekingduck.pipeline.nodes.model.movenet.movenet_model.logger"
+                ) as captured:
+
+                    movenet = Node(config=movenet_config)
+                    # records 0 - 20 records are updates to configs
+                    assert (
+                        captured.records[0].getMessage()
+                        == "---no movenet weights detected. proceeding to download...---"
+                    )
+                    assert (
+                        captured.records[1].getMessage()
+                        == "---movenet weights download complete.---"
+                    )
+                    assert movenet is not None
+
+    def test_no_human_single(self, empty_image, movenet_model_single):
+        no_human_img = cv2.imread(os.path.join(TEST_DIR, empty_image))
+        output = movenet_model_single.run({"img": no_human_img})
+        # expected_output = {
+        #     "bboxes": np.zeros((0, 4)),
+        #     "keypoints": np.zeros((0, 17, 2)),
+        #     "keypoint_scores": np.zeros((0, 17)),
+        #     "keypoint_conns": np.zeros(0),
+        #     "bbox_labels": np.zeros(0),
+        # }
+        expected_output = {
+            "bboxes": np.zeros(0),
+            "keypoints": np.zeros(0),
+            "keypoint_scores": np.zeros(0),
+            "keypoint_conns": np.zeros(0),
+            "bbox_labels": np.zeros(0),
+        }
+
+        assert output.keys() == expected_output.keys()
+        for i in expected_output.keys():
+            npt.assert_array_equal(
+                output[i], expected_output[i]
+            ), "unexpected output for {}".format(i)
+
+    def test_no_human_multi(self, empty_image, movenet_model_multi):
+        no_human_img = cv2.imread(os.path.join(TEST_DIR, empty_image))
+        output = movenet_model_multi.run({"img": no_human_img})
+        # expected_output = {
+        #     "bboxes": np.zeros((0, 4)),
+        #     "keypoints": np.zeros((0, 17, 2)),
+        #     "keypoint_scores": np.zeros((0, 17)),
+        #     "keypoint_conns": np.zeros(0),
+        #     "bbox_labels": np.zeros(0),
+        # }
+        expected_output = {
+            "bboxes": np.zeros(0),
+            "keypoints": np.zeros(0),
+            "keypoint_scores": np.zeros(0),
+            "keypoint_conns": np.zeros(0),
+            "bbox_labels": np.zeros(0),
+        }
+
+        assert output.keys() == expected_output.keys()
+        for i in expected_output.keys():
+            npt.assert_array_equal(
+                output[i], expected_output[i]
+            ), "unexpected output for {}".format(i)
+
+    def test_single_human(self, single_person_image, movenet_model_single):
+        single_human_img = cv2.imread(os.path.join(TEST_DIR, single_person_image))
+        output = movenet_model_single.run({"img": single_human_img})
+        expected_output = dict.fromkeys(
+            ["bboxes", "keypoints", "keypoint_scores", "keypoint_conns", "bbox_labels"]
+        )
+        assert output.keys() == expected_output.keys()
+        for i in expected_output.keys():
+            assert (
+                len(output[i]) == 1
+            ), "unexpected number of outputs for {} in singlepose".format(i)
+        assert output["bbox_labels"] == ["Person"]
+
+    def test_multi_human(self, multi_person_image, movenet_model_multi):
+        multi_human_img = cv2.imread(os.path.join(TEST_DIR, multi_person_image))
+        output = movenet_model_multi.run({"img": multi_human_img})
+        expected_output = dict.fromkeys(
+            ["bboxes", "keypoints", "keypoint_scores", "keypoint_conns", "bbox_labels"]
+        )
+        assert output.keys() == expected_output.keys()
+        for i in expected_output.keys():
+            assert (
+                len(output[i]) >= 2
+            ), "unexpected number of outputs for {} in multipose".format(i)
+
+        for label in output["bbox_labels"]:
+            assert label == "Person"
+
+    def test_bbox_score_threshold(self, movenet_config, invalid_thresholds):
+        movenet_config["bbox_score_threshold"] = invalid_thresholds
+        with pytest.raises(ValueError):
+            movenet = Node(movenet_config)
+
+    def test_keypoint_score_threshold(self, movenet_config, invalid_thresholds):
+        movenet_config["keypoint_score_threshold"] = invalid_thresholds
+        with pytest.raises(ValueError):
+            movenet = Node(movenet_config)
+
+    def test_model_type(self, movenet_config):
+        movenet_config["model_type"] = "not_movenet"
+        with pytest.raises(ValueError):
+            movenet = Node(movenet_config)
---
+++ tests/pipeline/nodes/model/movenetv1/test_predictor.py
@@ -0,0 +1,172 @@
+"""
+Copyright 2021 AI Singapore
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import pytest
+import yaml
+import cv2
+import os
+import numpy as np
+import numpy.testing as npt
+import tensorflow as tf
+from peekingduck.pipeline.nodes.model.movenet import Node
+from peekingduck.pipeline.nodes.model.movenetv1.movenet_files.predictor import Predictor
+
+TEST_DIR = os.path.join(os.getcwd(), "images", "testing")
+single_person_list = ["t2.jpg"]
+
+
+@pytest.fixture
+def movenet_config():
+    filepath = os.path.join(
+        os.getcwd(), "tests/pipeline/nodes/model/movenetv1/test_movenet.yml",
+    )
+    with open(filepath) as file:
+        node_config = yaml.safe_load(file)
+    node_config["root"] = os.getcwd()
+
+    return node_config
+
+
+@pytest.fixture
+def movenet(movenet_config):
+    node = Node(movenet_config)
+    return node
+
+
+@pytest.fixture
+def movenet_predictor(movenet_config):
+    predictor = Predictor(movenet_config)
+    return predictor
+
+
+@pytest.fixture(params=single_person_list)
+def single_person_image(request):
+    yield request.param
+
+
+@pytest.mark.mlmodel
+class TestPredictor:
+    def test_predictor(self):
+        assert movenet_predictor is not None, "Predictor is not instantiated"
+
+    def test_model_creation(self, movenet_predictor):
+        assert (
+            movenet_predictor._create_posenet_model is not None
+        ), "Model is not loaded"
+
+    def test_get_resolution_as_tuple(self, movenet_predictor):
+        resolution = {"height": 256, "width": 256}
+        tuple_res = movenet_predictor.get_resolution_as_tuple(resolution)
+        assert type(tuple_res) is tuple, "Resolution in config must be a tuple"
+        assert tuple_res == (256, 256), "Resolution is loaded incorrectly"
+        assert len(tuple_res) == 2, "Resolution in config must be a 2 length tuple"
+
+    def test_predict(self, movenet_predictor, single_person_image):
+        img = cv2.imread(os.path.join(TEST_DIR, single_person_image))
+        (
+            bboxes,
+            valid_keypoints,
+            keypoints_scores,
+            keypoints_conns,
+        ) = movenet_predictor.predict(img)
+        assert bboxes.shape == (1, 4)
+        assert valid_keypoints.shape == (1, 17, 2)
+        assert keypoints_scores.shape == (1, 17)
+        assert keypoints_conns.shape == (1, 19, 2, 2)
+
+    def test_get_results_single(self, movenet_predictor):
+        # prediction for singlepose model is in shape of [1,1,17,13]
+        # generates random tensor with values from 0.3 to 0.9
+        prediction = tf.random.uniform(
+            (1, 1, 17, 3), minval=0.3, maxval=0.9, dtype=tf.dtypes.float32, seed=24
+        )
+        (
+            bbox,
+            valid_keypoints,
+            keypoints_scores,
+            keypoints_conns,
+        ) = movenet_predictor._get_results_single(prediction)
+        assert bbox.shape == (1, 4)
+        assert valid_keypoints.shape == (1, 17, 2)
+        assert keypoints_scores.shape == (1, 17)
+        assert keypoints_conns.shape[0] == 1
+
+        # generates random tensor with values from 0.0 to 0.1
+        # since values are below config score threshold
+        # predictions should be tuples of empty np array
+        prediction_no_pose = tf.random.uniform(
+            (1, 1, 17, 3), minval=0.0, maxval=0.1, dtype=tf.dtypes.float32, seed=24
+        )
+        (
+            bbox_no_pose,
+            valid_keypoints_no_pose,
+            keypoints_scores_no_pose,
+            keypoints_conns_no_pose,
+        ) = movenet_predictor._get_results_single(prediction_no_pose)
+        npt.assert_array_equal(bbox_no_pose, np.zeros(0)), "unexpected output for bbox"
+        npt.assert_array_equal(
+            valid_keypoints_no_pose, np.zeros(0)
+        ), "unexpected output for keypoints"
+        npt.assert_array_equal(
+            keypoints_scores_no_pose, np.zeros(0)
+        ), "unexpected output for keypoint scores"
+        npt.assert_array_equal(
+            keypoints_conns_no_pose, np.zeros(0)
+        ), "unexpected output for keypoint connections"
+
+    def test_get_results_multi(self, movenet_predictor):
+        # prediction for multi model is in shape of [1,6,56]
+        # generates random tensor with values from 0.2 to 0.9
+        # since threshold in config is at 0.2, this random tensor
+        # will have at least 1 pose after filtering
+        prediction = tf.random.uniform(
+            (1, 6, 56), minval=0.3, maxval=0.9, dtype=tf.dtypes.float32, seed=24
+        )
+        (
+            bbox,
+            valid_keypoints,
+            keypoints_scores,
+            keypoints_conns,
+        ) = movenet_predictor._get_results_multi(prediction)
+        # output of random tensor will produce between
+        assert bbox.shape[0] >= 1
+        assert bbox.shape[0] <= 6
+        assert bbox.shape[0] == valid_keypoints.shape[0]
+        assert bbox.shape[0] == keypoints_scores.shape[0]
+        assert bbox.shape[0] == keypoints_conns.shape[0]
+
+        # generates random tensor with values from 0.0 to 0.1
+        # since values are below config score threshold
+        # predictions should be tuples of empty numpy arrays
+        # but with different shapes
+        prediction_no_pose = tf.random.uniform(
+            (1, 6, 56), minval=0.0, maxval=0.1, dtype=tf.dtypes.float32, seed=24
+        )
+        (
+            bbox_no_pose,
+            valid_keypoints_no_pose,
+            keypoints_scores_no_pose,
+            keypoints_conns_no_pose,
+        ) = movenet_predictor._get_results_multi(prediction_no_pose)
+        npt.assert_array_equal(bbox_no_pose, np.zeros(0)), "unexpected output for bbox"
+        npt.assert_array_equal(
+            valid_keypoints_no_pose, np.zeros(0)
+        ), "unexpected output for keypoints"
+        npt.assert_array_equal(
+            keypoints_scores_no_pose, np.zeros(0)
+        ), "unexpected output for keypoint scores"
+        npt.assert_array_equal(
+            keypoints_conns_no_pose, np.zeros(0)
+        ), "unexpected output for keypoint connections"
