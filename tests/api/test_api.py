1d25c11cc10cc9ffec68853622a5581703ff4a5e
dotw <davidong@aisingapore.org>
2021-11-24 16:48:43 +0800

    Add CI/CD tests for import peekingduck API mode

---
+++ tests/api/test_api.py
@@ -0,0 +1,101 @@
+# Copyright 2021 AI Singapore
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+To test "import peekingduck" python API mode
+"""
+
+import re
+import subprocess
+from pathlib import Path
+import pytest
+
+
+@pytest.mark.usefixtures("tmp_dir")
+class TestApi:
+    @staticmethod
+    def exec_code(code: str) -> str:
+        """Helper to execute given code string
+
+        Args:
+            code (str): python program to run
+
+        Returns:
+            str: output from running given code
+        """
+        temp_dir = Path.cwd()
+        with open("tmp.py", "w") as f:
+            f.writelines(code)
+        cmd = ["python", "tmp.py"]
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=temp_dir)
+        out, err = proc.communicate()
+        assert err is None  # no errors at all
+        out_str = out.decode("utf-8")
+        return out_str
+
+    def test_version(self):
+        """
+        Check we get a valid version number
+        """
+        code = """
+from peekingduck import __version__ as ver
+print(ver)
+        """
+        output = TestApi.exec_code(code)
+        res = re.match(r"v\d\.\d\.\d", output)
+        assert res is not None
+
+    def test_pkd_imports(self):
+        """
+        Check can import without errors
+        """
+        code = """
+from peekingduck import cli
+from peekingduck.configloader import ConfigLoader
+from peekingduck.declarative_loader import DeclarativeLoader
+from peekingduck.runner import Runner
+from peekingduck.pipeline.pipeline import Pipeline
+from peekingduck.pipeline.nodes.node import AbstractNode
+print("good")
+        """
+        output = TestApi.exec_code(code)
+        res = re.match(r"good", output)
+        assert res is not None
+
+    def test_pkd_import_types(self):
+        """
+        Check imports are of the right (class) types
+        """
+        code = """
+from peekingduck import cli
+from peekingduck.configloader import ConfigLoader
+from peekingduck.declarative_loader import DeclarativeLoader
+from peekingduck.runner import Runner
+from peekingduck.pipeline.pipeline import Pipeline
+from peekingduck.pipeline.nodes.node import AbstractNode
+import inspect
+the_types = []
+the_types.append(inspect.isclass(cli))
+the_types.append(inspect.isclass(ConfigLoader))
+the_types.append(inspect.isclass(DeclarativeLoader))
+the_types.append(inspect.isclass(Runner))
+the_types.append(inspect.isclass(Pipeline))
+the_types.append(inspect.isclass(AbstractNode))
+the_types_str = "".join([str(x) for x in the_types])
+print(the_types_str)
+        """
+        output = TestApi.exec_code(code)
+        print(output)
+        res = re.match(r"FalseTrueTrueTrueTrueTrue", output)
+        assert res is not None
