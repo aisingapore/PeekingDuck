e2b0672579b41c00b67012c8a49540f12648c8b9
yier <56420072+liyier90@users.noreply.github.com>
2021-12-17 12:09:00 +0800

    refactor: remove training related code such as loss, replace some custom layers with pytorch equivalent, unnested util scripts, and update documentation

--- peekingduck/pipeline/nodes/model/jdev1/jde_model.py
+++ peekingduck/pipeline/nodes/model/jdev1/jde_model.py
@@ -32,157 +32,72 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""
-JDE model for human detection and tracking.
-"""
+"""JDE model for human detection and tracking."""
 
-from typing import Any, Dict, List, Tuple
 import logging
+from typing import Any, Dict, List, Tuple
+
 import numpy as np
-import cv2
-import torch
-from peekingduck.pipeline.nodes.model.jdev1.jde_files.utils.utils import letterbox
-from peekingduck.pipeline.nodes.model.jdev1.jde_files.utils.parse_config import (
-    parse_model_cfg,
-)
-from peekingduck.pipeline.nodes.model.jdev1.jde_files.multitracker import JDETracker
+
+from peekingduck.pipeline.nodes.model.jdev1.jde_files.tracker import Tracker
 from peekingduck.weights_utils import checker, downloader, finder
 
 
 class JDEModel:  # pylint: disable=too-few-public-methods
-    """JDE model that detects and tracks humans."""
+    """JDE Model with model types: 576x320, 865x480, and 1088x608.
 
-    def __init__(self, config: Dict[str, Any]) -> None:
+    Args:
+        config (Dict[str, Any]): Model configuration options.
+        frame_rate (float): The frame rate of the current video sequence,
+            used for computing the size of track buffer.
+
+    Raises:
+        ValueError: `iou_threshold` is beyond [0, 1].
+        ValueError: `nms_threshold` is beyond [0, 1].
+        ValueError: `score_threshold` is beyond [0, 1].
+    """
+
+    def __init__(self, config: Dict[str, Any], frame_rate: float) -> None:
         self.logger = logging.getLogger(__name__)
-
         # Check threshold values
         if not 0 <= config["iou_threshold"] <= 1:
             raise ValueError("iou_threshold must be in [0, 1]")
-        if not 0 <= config["score_threshold"] <= 1:
-            raise ValueError("score_threshold must be in [0, 1]")
         if not 0 <= config["nms_threshold"] <= 1:
             raise ValueError("nms_threshold must be in [0, 1]")
-        self.config = config
+        if not 0 <= config["score_threshold"] <= 1:
+            raise ValueError("score_threshold must be in [0, 1]")
 
-        # Check for JDE weights
+        # Check for weights
         weights_dir, model_dir = finder.find_paths(
             config["root"], config["weights"], config["weights_parent_dir"]
         )
-        # pylint: disable=logging-fstring-interpolation
         if not checker.has_weights(weights_dir, model_dir):
             self.logger.info("No weights detected. Proceeding to download...")
             downloader.download_weights(weights_dir, config["weights"]["blob_file"])
             self.logger.info(f"Weights downloaded to {weights_dir}.")
 
-        self.config["cfg"] = model_dir / config["weights"]["config_file"]
-        self.config["weights_path"] = str(
-            model_dir / config["weights"]["model_file"]["jde"]
-        )
-        cfg_dict = parse_model_cfg(self.config["cfg"])
-        self.config["img_size"] = [
-            int(cfg_dict[0]["width"]),
-            int(cfg_dict[0]["height"]),
-        ]
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.tracker = JDETracker(self.config, self.device)
+        self.tracker = Tracker(config, model_dir, frame_rate)
 
-    def predict(self, frame: np.ndarray) -> Tuple[np.ndarray, np.ndarray, List[str]]:
-        """Predicts person bboxes, object_id and labels.
+    def predict(
+        self, image: np.ndarray
+    ) -> Tuple[List[np.ndarray], List[str], List[float], List[str]]:
+        """Track objects from image.
 
         Args:
-            frame (np.ndarray): image in numpy array.
+            image (np.ndarray): Image in numpy array.
 
         Returns:
-            Tuple[np.ndarray, np.ndarray, List[str]]: Tuple containing
-                detected bounding boxes, object tracking id and "person"
-                label for each bounding box respectively.
+            (Tuple[List[np.ndarray], List[str], List[float]]): A tuple of
+            - Numpy array of detected bounding boxes.
+            - List of detection class labels (person).
+            - List of detection confidence scores.
+            - List of track IDs.
+
+        Raises:
+            TypeError: The provided `image` is not a numpy array.
         """
-        img, img0 = self._reshape_image(frame)
-
-        # Run detection and tracking
-        blob = torch.from_numpy(img).unsqueeze(0).to(self.device)
-        online_targets = self.tracker.update(blob, img0)
-        online_tlwhs = []
-        online_ids = []
-        for track in online_targets:
-            tlwh = track.tlwh
-            tid = track.track_id
-            vertical = tlwh[2] / tlwh[3] > 1.6
-            if tlwh[2] * tlwh[3] > self.config["min_box_area"] and not vertical:
-                online_tlwhs.append(self._normalize_bboxes(tlwh))
-                online_ids.append(str(tid))
-
-        output_len = len(online_tlwhs)
-        labels = ["person"] * output_len
-        return online_tlwhs, online_ids, labels
-
-    # pylint: disable=attribute-defined-outside-init
-    def _reshape_image(self, frame: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """Resizes image to the specified size in model config file.
-
-        Args:
-            frame (np.ndarray): Frame to reshape.
-
-        Returns:
-            Tuple[np.ndarray, np.ndarray]: Normalized, and resized image.
-        """
-        img_width = self.config["img_size"][0]
-        img_height = self.config["img_size"][1]
-
-        frame_height, frame_width, _ = frame.shape
-        self.width, self.height = self._get_size(
-            frame_width, frame_height, img_width, img_height
-        )
-        img0 = cv2.resize(frame, (self.width, self.height))
-        # Padded resize
-        img, _, _, _ = letterbox(img0, height=img_height, width=img_width)
-
-        # Normalize RGB
-        img = img[:, :, ::-1].transpose(2, 0, 1)
-        img = np.ascontiguousarray(img, dtype=np.float32)
-        img /= 255.0
-        return img, img0
-
-    @staticmethod
-    def _get_size(
-        frame_width: int, frame_height: int, img_width: int, img_height: int
-    ) -> Tuple[int, int]:
-        """Returns the requested size of image.
-
-        Args:
-            frame_width (int): Width of frame.
-            frame_height (int): Height of frame.
-            img_width (int): Image size width.
-            img_height (int): Image size height.
-
-        Returns:
-            Tuple[int, int]: The requested size in pixels, as a 2-tuple:
-                (width, height).
-        """
-        width_ratio = float(img_width) / frame_width
-        height_ratio = float(img_height) / frame_height
-        aspect_ratio = min(width_ratio, height_ratio)
-        return int(frame_width * aspect_ratio), int(frame_height * aspect_ratio)
-
-    def _normalize_bboxes(self, tlwh: np.ndarray) -> np.ndarray:
-        """Normalizes coordinates of a bounding box. Divides x-coordinates
-        by image width and y-coordinates by image height.
-
-        Args:
-            tlwh (np.ndarray): Bounding box in format
-                `(top left x, top left y, width, height)`.
-
-        Returns:
-            np.ndarray: Normalized bounding box in format
-                `(top left x, top left y, bottom right x, bottom right y)`.
-        """
-        ret = np.asarray(tlwh).copy()
-        ret[2:] += ret[:2]
-        tlx, tly, blx, bly = ret
-
-        n_tlx = tlx / self.width
-        n_tly = tly / self.height
-        n_blx = blx / self.width
-        n_bly = bly / self.height
-
-        return np.array([n_tlx, n_tly, n_blx, n_bly])
+        if not isinstance(image, np.ndarray):
+            raise TypeError("image must be a np.ndarray")
+        bboxes, track_ids, bbox_scores = self.tracker.track_objects_from_image(image)
+        bbox_labels = ["person"] * len(bboxes)
+        return bboxes, bbox_labels, bbox_scores, track_ids
