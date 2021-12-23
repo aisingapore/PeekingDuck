a8050972033ff7e5030137348826ff8d88625dd1
dotw <davidong@aisingapore.org>
2021-12-23 12:39:44 +0800

    Add unit tests for wildcard for object detection models

--- tests/loaders/test_configloader.py
+++ tests/loaders/test_configloader.py
@@ -95,6 +95,30 @@ class TestConfigLoader:
         assert test_map["clock"] == 74
         assert test_map["toothbrush"] == 79
 
+    def test_config_loader_obj_det_wildcard_efficientdet(self, configloader):
+        """Test object detection wildcard for EfficientDet models"""
+        node_name = "model.efficientdet"
+        key = "detect_ids"
+        val = ["*"]
+        # fmt: off
+        ground_truth = ("detect_ids", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 27, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 69, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 87, 88, 89])
+        # fmt: on
+
+        test_res = configloader.change_class_name_to_id(node_name, key, val)
+        assert test_res == ground_truth
+
+    def test_config_loader_obj_det_wildcard_yolo(self, configloader):
+        """Test object detection wildcard for Yolo models"""
+        node_name = "model.yolo"
+        key = "detect_ids"
+        val = ["*"]
+        # fmt: off
+        ground_truth = ("detect_ids", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79])
+        # fmt: on
+
+        test_res = configloader.change_class_name_to_id(node_name, key, val)
+        assert test_res == ground_truth
+
     #
     # Test class name to object ID translation for different input cases
     # NB: We are not testing model operations here (that is done under model tests).
