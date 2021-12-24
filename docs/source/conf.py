7fb0b915bd03550c8ade3dc82146b663c370c761
yier <56420072+liyier90@users.noreply.github.com>
2021-12-24 10:22:25 +0800

    docs: change conf.py to ignore jdev1 instead of jde_mot

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,7 +83,7 @@ autosummary_mock_imports = [
     "peekingduck.pipeline.nodes.model.csrnetv1"
     "peekingduck.pipeline.nodes.model.efficientdet_d04",
     "peekingduck.pipeline.nodes.model.hrnetv1",
-    "peekingduck.pipeline.nodes.model.jde_mot",
+    "peekingduck.pipeline.nodes.model.jdev1",
     "peekingduck.pipeline.nodes.model.mtcnnv1",
     "peekingduck.pipeline.nodes.model.posenetv1",
     "peekingduck.pipeline.nodes.model.movenetv1",
