950a8d73bd143ad53f8b5931776c97a6e21de789
yier <56420072+liyier90@users.noreply.github.com>
2021-12-29 14:29:50 +0800

    refactor: call TrackerMOSSE_create through cv2.legacy

--- peekingduck/pipeline/nodes/dabble/trackingv1/tracking_files/opencv_tracker.py
+++ peekingduck/pipeline/nodes/dabble/trackingv1/tracking_files/opencv_tracker.py
@@ -76,7 +76,7 @@ class OpenCVTracker:  # pylint: disable=too-few-public-methods
             frame (np.ndarray): Image frame parsed from video.
             bbox (np.ndarray): Single detected bounding box.
         """
-        tracker = cv2.TrackerMOSSE_create()
+        tracker = cv2.legacy.TrackerMOSSE_create()
         tracker.init(frame, tuple(bbox))
         self.tracks[self.next_track_id] = Track(tracker, bbox)
         self.next_track_id += 1
@@ -147,5 +147,5 @@ class Track(NamedTuple):
             height of the bounding box.
     """
 
-    tracker: cv2.TrackerMOSSE
+    tracker: cv2.legacy_TrackerMOSSE
     bbox: np.ndarray
