b8631181385904fbea195fd9ad3598cfe5135045
leeping-ng <leeping_ng@hotmail.com>
2022-02-24 17:44:57 +0800

    node: working implementation of dabble.stats with single line expression

--- peekingduck/pipeline/nodes/dabble/statistics.py
+++ peekingduck/pipeline/nodes/dabble/statistics.py
@@ -17,20 +17,23 @@ A flexible node created for calculating the average, minimum and maximum of a si
 interest over time.
 """
 
-from collections import deque
+from collections import deque, OrderedDict
 import operator
-
+import re
 from typing import Any, Dict, List, Tuple, Type, Union
 
 from peekingduck.pipeline.nodes.node import AbstractNode
 
-OPS = {
-    "==": operator.eq,
-    ">": operator.gt,
-    ">=": operator.ge,
-    "<": operator.lt,
-    "<=": operator.le,
-}
+# Order matters so that regex doesn't read ">=" as ">" or "<=" as "<"
+OPS = OrderedDict(
+    {
+        ">=": operator.ge,
+        ">": operator.gt,
+        "==": operator.eq,
+        "<=": operator.le,
+        "<": operator.lt,
+    }
+)
 
 
 class Node(AbstractNode):
@@ -159,6 +162,18 @@ class Node(AbstractNode):
         super().__init__(config, node_path=__name__, **kwargs)
         self.avg, self.min, self.max = 0.0, float("inf"), 0
         self.num_iter = 0
+        all_methods = {
+            "conditional_count": self.conditional_count,
+            "identity": self.identity,
+            "length": self.length,
+            "minimum": self.minimum,
+            "maximum": self.maximum,
+        }
+        self.method, self.expr = _get_method_expr(all_methods)
+
+        self.data_type, self.keys, self.condition = _get_run_data(
+            self.method, self.expr
+        )
 
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         """Calculates the average, minimum and maximum of a single variable of interest over time.
@@ -169,8 +184,8 @@ class Node(AbstractNode):
         Returns:
             outputs (dict): Dictionary with keys "avg", "min" and "max".
         """
-        target_attr = self._get_target_attr(inputs)
-        self.curr = _apply_func(target_attr, self.apply)
+        target_attr = _get_value(inputs[self.data_type], self.keys.copy())
+        self.curr = _apply_func(target_attr, self.method, self.condition)
 
         # if no detections in this frame, return stats from previous detections
         if not self.curr:
@@ -180,13 +195,6 @@ class Node(AbstractNode):
         self._update_stats(self.curr)
 
         return {"avg": self.avg, "min": self.min, "max": self.max}
-
-    def _get_target_attr(self, inputs: Dict[str, Any]) -> Union[int, float, list, dict]:
-        """Gets the target attributes from inputs."""
-        if self.target["get"]:
-            keys = deque(self.target["get"])
-            return _get_value(inputs[self.target["data_type"]], keys)
-        return inputs[self.target["data_type"]]
 
     def _update_stats(self, curr: Union[float, int]) -> None:
         """Updates the avg, min and max values with the current value."""
@@ -202,41 +210,129 @@ class Node(AbstractNode):
         self.avg = (self.avg * self.num_iter + curr) / (self.num_iter + 1)
 
 
+def _get_method_expr(all_methods: Dict[str, Union[str, None]]) -> Tuple[str, str]:
+    """Gets the only non-null key value pair from a dictionary."""
+    num_methods = 0
+    for method, expr in all_methods.items():
+        if expr:
+            num_methods += 1
+            selected_method, selected_expr = method, expr
+
+    if num_methods < 1:
+        raise ValueError(
+            f"No methods selected in config, but one method needs to be selected to proceed."
+        )
+    elif num_methods > 1:
+        raise ValueError(
+            f"{num_methods} methods selected in config, but only one method should be selected "
+            f"to proceed."
+        )
+    return selected_method, selected_expr
+
+
+def _get_run_data(method: str, expr: str) -> Tuple[str, List[str], Dict[str, Any]]:
+
+    """
+    "obj_attrs[ids]"
+    "obj_attrs[flags] == TOO CLOSE!"
+    "large_groups >= 2"
+
+    target_attr __ operator __ operand
+
+    """
+    ops_expr = "(" + ")|(".join(OPS.keys()) + ")"
+    match = re.search(ops_expr, expr)
+    if not match and method == "conditional_count":
+        raise ValueError(
+            f"The chosen method: {method} should have an operator for comparison."
+        )
+    if match and method != "conditional_count":
+        raise ValueError(
+            f"The chosen method: {method} should not have the {match.group()} "
+            f"operator."
+        )
+
+    condition = {"op_func": None, "operand": None}
+    if match:
+        condition["op_func"] = OPS[match.group()]
+        op_idx_start, op_idx_end = match.span()
+        target_attr = expr[:op_idx_start]
+        # check int/float/str for operand
+        operand_raw = expr[op_idx_end + 1 :]
+        condition["operand"] = _get_operand(operand_raw)
+    else:
+        target_attr = expr
+
+    data_type, keys = _get_data_type_and_keys(target_attr)
+
+    return data_type, keys, condition
+
+
+def _get_data_type_and_keys(target_attr: str) -> Tuple[str, List[Union[str, None]]]:
+    target_attr = re.sub("'|\"", "", target_attr)
+    target_attr = target_attr.strip()
+    keys = re.findall("\[(.*?)\]", target_attr)
+    if not keys:
+        data_type = target_attr
+        keys = []
+    else:
+        data_type = target_attr.split("[")[0]
+
+    return data_type, keys
+
+
+def _get_operand(operand_raw: str) -> Union[str, float]:
+    operand_raw = operand_raw.strip()
+    is_string = re.findall("'|\"", operand_raw)
+    if is_string:
+        operand = re.sub("'|\"", "", operand_raw)
+        return operand
+    try:
+        return float(operand_raw)
+    except:
+        raise TypeError(
+            f"The detected operand here is: {operand_raw}."
+            f"If the operand is intended to be a string, ensure that it is enclosed by single "
+            f"or double quotes. Else, it is assumed to be of integer or float type, and will be "
+            f"subsequently converted into a float."
+        )
+
+
 def _get_value(data: Dict[str, Any], keys: deque) -> Union[int, float, list, dict]:
     """Recursively goes through the keys of a dictionary to obtain the final value."""
     if not keys:
         return data
-    key = keys.popleft()
+    key = keys.pop(0)
     return _get_value(data[key], keys)
 
 
 def _apply_func(
-    target_attr: Union[int, float, list, dict], apply: Dict[str, Any]
+    target_attr: Union[int, float, list, dict],
+    method: str,
+    condition: Dict[str, Any],
 ) -> Union[int, float, None]:
-    """Applies a function and optional condition to a target attribute."""
-    function = apply["function"]
-    condition = apply["condition"]
+    """Applies a method and optional condition to a target attribute."""
     if not target_attr:
         return None
-    if function == "identity":
-        _check_type(target_attr, function, int, float)
+    if method == "identity":
+        _check_type(target_attr, method, int, float)
         return target_attr
-    if function == "len":
-        _check_type(target_attr, function, dict, list)
+    if method == "length":
+        _check_type(target_attr, method, dict, list)
         return len(target_attr)
-    if function == "min":
-        _check_type(target_attr, function, dict, list)
+    if method == "minimum":
+        _check_type(target_attr, method, dict, list)
         return min(target_attr)
-    if function == "max":
-        _check_type(target_attr, function, dict, list)
+    if method == "maximum":
+        _check_type(target_attr, method, dict, list)
         return max(target_attr)
-    # if function == "conditional_count"
-    _check_type(target_attr, function, list)
+    # if method == "conditional_count"
+    _check_type(target_attr, method, list)
     return _conditional_count(target_attr, condition)
 
 
 def _check_type(
-    target_attr: Union[int, float, list, dict], function: str, *types: type
+    target_attr: Union[int, float, list, dict], method: str, *types: type
 ) -> None:
     """Checks that a target attribute conforms to the defined type(s)."""
     correct = False
@@ -245,18 +341,35 @@ def _check_type(
             correct = True
     if not correct:
         raise ValueError(
-            f"For the chosen function: '{function}', valid target attribute types are: {types}. "
+            f"For the chosen method: '{method}', valid target attribute types are: {types}. "
             f"However, this target attribute: {target_attr} is of type: {type(target_attr)}."
         )
 
 
 def _conditional_count(
-    target_attr: Union[int, float, List[Any], Dict[str, Any]], condition: Dict[str, str]
+    target_attr: Union[int, float, List[Any], Dict[str, Any]], condition: Dict[str, Any]
 ) -> int:
     """Counts the number of elements in a list given an operator and operand for comparison."""
     count = 0
-    op_func = OPS[condition["operator"]]
     for item in target_attr:
-        if op_func(item, condition["operand"]):
+        if condition["op_func"](item, condition["operand"]):
             count += 1
     return count
+
+
+"""
+Rules:
+
+a. String operands need to be wrapped in single/double quotes
+b. Spaces in the expression will be ignored except for string operands
+
+Say that don't put stuff in square brackets, we using regex
+Unit tests
+
+1. All operators work
+2. Don't accept multiple operators
+3. Spaces not included
+4. 4 nulls and only 1 mode not null
+5. "'asdasd'" and '"asfgasf"'
+
+"""
