719f5b474162a6f78efb5e2ca4686ac75f18eecc
yier <56420072+liyier90@users.noreply.github.com>
2022-03-23 17:43:19 +0800

    docs: fix typo in module docstring

--- peekingduck/pipeline/nodes/output/csv_writer.py
+++ peekingduck/pipeline/nodes/output/csv_writer.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Records the nodes outputs to a CSV file.
-"""
+"""Records the nodes' outputs to a CSV file."""
 
 import logging
 import textwrap
