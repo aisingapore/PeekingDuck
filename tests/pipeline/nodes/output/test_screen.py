d80816e37a01127ac9b7c4fb42dd264d3316be4f
yier <56420072+liyier90@users.noreply.github.com>
2022-08-19 16:49:55 +0800

    test: add test for output.screen initial run()

--- tests/pipeline/nodes/output/test_screen.py
+++ tests/pipeline/nodes/output/test_screen.py
@@ -13,11 +13,23 @@
 # limitations under the License.
 
 import platform
+from unittest import mock
 
 import cv2
 import pytest
+import yaml
 
-from peekingduck.pipeline.nodes.output.screen import Node
+from peekingduck.pipeline.nodes.output.screen import MIN_DISPLAY_SIZE, Node
+from tests.conftest import PKD_DIR
+
+
+@pytest.fixture
+def screen_config():
+    with open(PKD_DIR / "configs" / "output" / "screen.yml") as infile:
+        node_config = yaml.safe_load(infile)
+    node_config["pkd_viewer"] = False
+
+    return node_config
 
 
 @pytest.fixture
@@ -34,15 +46,57 @@ class TestScreen:
         prop = cv2.getWindowProperty(node.window_name, cv2.WND_PROP_VISIBLE)
         assert int(prop) == 1
 
-    def test_viewer_disables_screen(self):
-        node = Node(
-            {
-                "window_name": "PeekingDuck",
-                "window_size": {"do_resizing": False, "width": 1280, "height": 720},
-                "window_loc": {"x": 0, "y": 0},
-                "pkd_viewer": True,
-            }
-        )
+    def test_viewer_disables_screen(self, screen_config):
+        screen_config["pkd_viewer"] = True
+        node = Node(screen_config)
         with pytest.raises(cv2.error) as excinfo:
             cv2.getWindowProperty(node.window_name, cv2.WND_PROP_VISIBLE)
         assert "please create a window" in str(excinfo)
+
+    def test_resize_to_image(self, create_input_image):
+        node = Node()
+        filename = "image1.png"
+        width = 800
+        height = 900
+        image = create_input_image(filename, (height, width, 3))
+        inputs = {"filename": filename, "img": image}
+        with mock.patch("cv2.resizeWindow") as mock_resize:
+            node.run(inputs)
+
+        assert mock_resize.call_args == ((node.window_name, width, height),)
+
+    def test_resize_to_config(self, screen_config, create_input_image):
+        width = 800
+        height = 900
+        screen_config["window_size"] = {
+            "do_resizing": True,
+            "height": height,
+            "width": width,
+        }
+        node = Node(screen_config)
+        filename = "image1.png"
+        image = create_input_image(filename, (height // 2, width // 2, 3))
+        inputs = {"filename": filename, "img": image}
+        with mock.patch("cv2.resizeWindow") as mock_resize:
+            node.run(inputs)
+
+        assert mock_resize.call_args == ((node.window_name, width, height),)
+
+    def test_resize_to_min_size(self, screen_config, create_input_image):
+        width = MIN_DISPLAY_SIZE // 2
+        height = MIN_DISPLAY_SIZE // 2
+        screen_config["window_size"] = {
+            "do_resizing": True,
+            "height": height,
+            "width": width,
+        }
+        node = Node(screen_config)
+        filename = "image1.png"
+        image = create_input_image(filename, (height // 2, width // 2, 3))
+        inputs = {"filename": filename, "img": image}
+        with mock.patch("cv2.resizeWindow") as mock_resize:
+            node.run(inputs)
+
+        assert mock_resize.call_args == (
+            (node.window_name, MIN_DISPLAY_SIZE, MIN_DISPLAY_SIZE),
+        )
