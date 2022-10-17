e1a65167417f1491008a3e41c53c5d2aa37e8bf2
yier <56420072+liyier90@users.noreply.github.com>
2022-10-17 09:16:24 +0800

    refactor: remove unnecessary try except as post_init() is guaranteed by base MediaPipeModel

--- peekingduck/pipeline/nodes/model/mediapipe_hub.py
+++ peekingduck/pipeline/nodes/model/mediapipe_hub.py
@@ -91,10 +91,7 @@ class Node(ThresholdCheckerMixin, AbstractNode):
         self.check_valid_choice("task", {"object_detection", "pose_estimation"})
 
         self.model = self.model_constructor[self.config["task"]](self.config)
-        try:
-            self.model.post_init()
-        except AttributeError:
-            pass
+        self.model.post_init()
         self._finalize_output_keys()
 
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
