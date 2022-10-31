cda99ba475652d38f7a8e4d81c53b3889301d7ce
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-10-31 15:13:56 +0800

    refactor/test: initialize empty keypoints, scores and conns with the correct empty shape in `test_no_poses`

--- tests/pipeline/nodes/draw/test_poses.py
+++ tests/pipeline/nodes/draw/test_poses.py
@@ -39,16 +39,19 @@ def draw_poses():
 
 class TestPoses:
     def test_no_poses(self, draw_poses, create_image):
-        poses = np.empty((0, 2))
-        scores = []
-        keypoint_conns = []
         original_img = create_image((28, 28, 3))
         output_img = original_img.copy()
-        input1 = {
-            "keypoints": poses,
-            "keypoint_scores": scores,
+
+        keypoints = np.empty((0, 17, 2))  # keypoints shape
+        keypoint_scores = np.empty((0, 17))  # keypoint_scores shape
+        keypoint_conns = np.empty((0, 15, 2, 2))  # keypoint_conns shape
+
+        inputs = {
+            "keypoints": keypoints,
+            "keypoint_scores": keypoint_scores,
             "keypoint_conns": keypoint_conns,
             "img": output_img,
         }
-        draw_poses.run(input1)
+        draw_poses.run(inputs)
+
         np.testing.assert_equal(original_img, output_img)
