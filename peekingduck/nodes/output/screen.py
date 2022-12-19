2186c5adbf839bcddfba0c0544fcef881da19b15
Teck Meng <furyx@hotmail.com>
2022-12-19 14:32:09 +0800

    feat: default image_waitkey to false

--- peekingduck/nodes/output/screen.py
+++ peekingduck/nodes/output/screen.py
@@ -89,8 +89,9 @@ class Node(AbstractNode):
         return {"pipeline_end": False}
 
     def _wait_for_keyboard_input(self, filename: str) -> int:
-        wait_ms = 1  # Delay in milliseconds
-        if self.window_image_wait and filename.split(".")[-1] in [
+        """check config['output'] image_waitkey whether to wait for user input before pipeline end"""
+        wait_ms = 1  # Default - Delay in milliseconds
+        if self.image_waitkey and filename.split(".")[-1] in [
             "jpg",
             "jpeg",
             "png",
