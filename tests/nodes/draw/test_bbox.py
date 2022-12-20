ea53ed73cc1dbcce5cd16fc57374084536c3c1e3
frankcaoyun <frankprvt@gmail.com>
2022-12-20 14:33:12 +0800

    fix: include the new config settings for show_scores and color_choice to resolve the issue in unit test.

--- tests/nodes/draw/test_bbox.py
+++ tests/nodes/draw/test_bbox.py
@@ -33,6 +33,8 @@ def draw_bbox_no_labels():
             "input": ["bboxes", "img", "bbox_labels", "bbox_scores"],
             "output": ["none"],
             "show_labels": False,
+            "show_scores": False,
+            "color_choice": [],
         }
     )
     return node
@@ -45,6 +47,8 @@ def draw_bbox_show_labels():
             "input": ["bboxes", "img", "bbox_labels", "bbox_scores"],
             "output": ["none"],
             "show_labels": True,
+            "show_scores": False,
+            "color_choice": [],
         }
     )
     return node
@@ -56,7 +60,13 @@ class TestBbox:
         original_img = create_image((28, 28, 3))
         output_img = original_img.copy()
         no_labels = []
-        input1 = {"bboxes": no_bboxes, "img": output_img, "bbox_labels": no_labels}
+        no_scores = []
+        input1 = {
+            "bboxes": no_bboxes,
+            "img": output_img,
+            "bbox_labels": no_labels,
+            "bbox_scores": no_scores,
+        }
         draw_bbox_no_labels.run(input1)
         np.testing.assert_equal(original_img, output_img)
 
@@ -66,8 +76,19 @@ class TestBbox:
         output_img_no_label = original_img.copy()
         output_img_show_label = original_img.copy()
         labels = ["Person"]
-        input1 = {"bboxes": bboxes, "img": output_img_no_label, "bbox_labels": labels}
-        input2 = {"bboxes": bboxes, "img": output_img_show_label, "bbox_labels": labels}
+        scores = [0.99]
+        input1 = {
+            "bboxes": bboxes,
+            "img": output_img_no_label,
+            "bbox_labels": labels,
+            "bbox_scores": scores,
+        }
+        input2 = {
+            "bboxes": bboxes,
+            "img": output_img_show_label,
+            "bbox_labels": labels,
+            "bbox_scores": scores,
+        }
         draw_bbox_no_labels.run(input1)
         # after running draw, should not be equal
         assert original_img.shape == output_img_no_label.shape
