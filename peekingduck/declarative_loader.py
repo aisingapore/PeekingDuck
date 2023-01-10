bc5a10b9302c597230a2410514caaa69ce0d3a11
yier <56420072+liyier90@users.noreply.github.com>
2023-01-10 09:43:12 +0800

    feat: remove input node deprecation handling

--- peekingduck/declarative_loader.py
+++ peekingduck/declarative_loader.py
@@ -151,51 +151,6 @@ class DeclarativeLoader:  # pylint: disable=too-few-public-methods, too-many-ins
         key = "detect"
         return key, value
 
-    def _handle_input_node_deprecation(
-        self, nodes: List[Union[str, Dict[str, Any]]]
-    ) -> List[Union[str, Dict[str, Any]]]:
-        """dotw 2022-03-16: Temporary code to convert existing `input.live` and
-        `input.recorded` into new `input.visual`. To be removed in subsequent
-        versions.
-        """
-
-        # dotw 2022-03-17: Temporary helper methods
-        def input_node_deprecation_warning(
-            name: str, config: Union[str, Dict[str, Any]]
-        ) -> None:
-            deprecate(
-                f"`{name}` deprecated and will be removed in the future. "
-                "Please use `input.visual` instead.",
-                4,
-            )
-            self.logger.warning(f"convert `{name}` to `input.visual`: {config}")
-
-        upgraded_nodes = []
-        for node in nodes:
-            if isinstance(node, str):
-                if node in ["input.live", "input.recorded"]:
-                    input_node_deprecation_warning(node, "input.visual")
-                    if node == "input.live":
-                        node = {"input.visual": {"source": 0}}
-                    else:
-                        self.logger.error("input.recorded with no parameters error!")
-                        node = "input.visual"
-            else:
-                if "input.live" in node:
-                    node_config = node.pop("input.live")
-                    if "input_source" in node_config:
-                        node_config["source"] = node_config.pop("input_source")
-                    node["input.visual"] = node_config
-                    input_node_deprecation_warning("input.live", node_config)
-                if "input.recorded" in node:
-                    node_config = node.pop("input.recorded")
-                    if "input_dir" in node_config:
-                        node_config["source"] = node_config.pop("input_dir")
-                    node["input.visual"] = node_config
-                    input_node_deprecation_warning("input.recorded", node_config)
-            upgraded_nodes.append(node)
-        return upgraded_nodes
-
     def _init_node(
         self,
         path_to_node: str,
@@ -272,10 +227,8 @@ class DeclarativeLoader:  # pylint: disable=too-few-public-methods, too-many-ins
         if nodes is None:
             raise ValueError(f"{pipeline_path} does not contain any nodes!")
 
-        upgraded_nodes = self._handle_input_node_deprecation(nodes)
-
         self.logger.info("Successfully loaded pipeline file.")
-        return NodeList(upgraded_nodes)
+        return NodeList(nodes)
 
     @staticmethod
     def _change_class_name_to_id(node_name: str, value: List[Any]) -> List[int]:
