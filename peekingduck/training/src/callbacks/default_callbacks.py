e2bc4ae6ac85f0e0dccf8a739afe7656f4316047
frankcaoyun <frankprvt@gmail.com>
2023-01-10 16:13:40 +0800

    feat: add in scripts for callbacks. Needs further testing.

--- peekingduck/training/src/callbacks/default_callbacks.py
+++ peekingduck/training/src/callbacks/default_callbacks.py
@@ -12,9 +12,114 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import ABC, abstractmethod
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING  # This solves circular import type hinting.
+
+if TYPE_CHECKING:
+    from src.trainer import Trainer
 
 
-class Callback(ABC):
+# pylint: disable=line-too-long, too-many-public-methods
+class Callback:
+    """Callback base class.
+
+    TODO: Torchflare sorts the callback, maybe we should look into it.
+    This might be because some default callbacks instantiate some global attributes
+    for eg, self.model_artifacts_dir
+
+    Reference:
+        - https://github.com/Lightning-AI/lightning/blob/master/src/pytorch_lightning/callbacks/callback.py
+        - https://github.com/Atharva-Phatak/torchflare/tree/main/torchflare
+    """
+
     def __init__(self) -> None:
-        pass
+        """Constructor for Callback base class."""
+
+    @property
+    def state_key(self) -> str:
+        """Identifier for the state of the callback.
+        Used to store and retrieve a callback's state from the checkpoint dictionary by
+        ``checkpoint["callbacks"][state_key]``. Implementations of a callback need to provide a unique state key if 1)
+        the callback has state and 2) it is desired to maintain the state of multiple instances of that callback.
+        """
+        return self.__class__.__qualname__
+
+    def _generate_state_key(self, **kwargs: Any) -> str:
+        """Formats a set of key-value pairs into a state key string with the callback class name prefixed. Useful
+        for defining a :attr:`state_key`.
+        Args:
+            **kwargs: A set of key-value pairs. Must be serializable to :class:`str`.
+        """
+        return f"{self.__class__.__qualname__}{repr(kwargs)}"
+
+    def setup(self, trainer: Trainer, stage: str) -> None:
+        """Called when fit, validate, test, predict, or tune begins."""
+
+    def teardown(self, trainer: Trainer, stage: str) -> None:
+        """Called when fit, validate, test, predict, or tune ends."""
+
+    def on_trainer_start(self, trainer: Trainer) -> None:
+        """Called when the trainer: Trainer begins."""
+
+    def on_trainer_end(self, trainer: Trainer) -> None:
+        """Called when the trainer: Trainer ends."""
+
+    def on_fit_start(self, trainer: Trainer) -> None:
+        """Called AFTER fit begins."""
+
+    def on_fit_end(self, trainer: Trainer) -> None:
+        """Called AFTER fit ends."""
+
+    def on_train_batch_start(self, trainer: Trainer) -> None:
+        """Called when the train batch begins."""
+
+    def on_train_batch_end(self, trainer: Trainer) -> None:
+        """Called when the train batch ends.
+        Note:
+            The value ``outputs["loss"]`` here will be the normalized value w.r.t
+            ``accumulate_grad_batches`` of the
+            loss returned from ``training_step``.
+        """
+
+    def on_train_loader_start(self, trainer: Trainer) -> None:
+        """Called when the train loader begins."""
+
+    def on_valid_loader_start(self, trainer: Trainer) -> None:
+        """Called when the validation loader begins."""
+
+    def on_train_loader_end(self, trainer: Trainer) -> None:
+        """Called when the train loader ends."""
+
+    def on_valid_loader_end(self, trainer: Trainer) -> None:
+        """Called when the validation loader ends."""
+
+    def on_train_epoch_start(self, trainer: Trainer) -> None:
+        """Called when the train epoch begins."""
+
+    def on_train_epoch_end(self, trainer: Trainer) -> None:
+        """Called when the train epoch ends.
+        To access all batch outputs at the end of the epoch, either:
+        1. Implement `training_epoch_end` in the `LightningModule` and access outputs via the module
+            OR
+        2. Cache data across train batch hooks inside the callback implementation to
+        post-process in this hook.
+        """
+
+    def on_valid_epoch_start(self, trainer: Trainer) -> None:
+        """Called when the val epoch begins."""
+
+    def on_valid_epoch_end(self, trainer: Trainer) -> None:
+        """Called when the val epoch ends."""
+
+    def on_valid_batch_start(self, trainer: Trainer) -> None:
+        """Called when the validation batch begins."""
+
+    def on_valid_batch_end(self, trainer: Trainer) -> None:
+        """Called when the validation batch ends."""
+
+    def on_inference_start(self, trainer: Trainer) -> None:
+        """Called when the inference begins."""
+
+    def on_inference_end(self, trainer: Trainer) -> None:
+        """Called when the inference ends."""
---
+++ peekingduck/training/src/callbacks/early_stopping.py
@@ -0,0 +1,75 @@
+"""Early Stopping Callbacks.
+
+Logic:
+    0. We keep track of the following variables:
+        - best_score: the best score so far, can be loss or metric.
+        - patience: the number of epochs to wait before stopping, if the score
+            does not improve.
+    1. This is called after each validation epoch.
+    2. We check if the current epoch score is better than the best score.
+    3. If it is, we change the best score to the current score and reset the patience.
+    4. If it is not, we decrement the patience.
+    5. If the patience is 0, we stop the training.
+
+TODO:
+    1. As usual, the issue is attributes/args such as mode is defined in `pipeline_config`,
+        may need to inherit the `pipeline_config` and use it to initiate the attributes.
+"""
+from src.callbacks.default_callbacks import Callback
+from src.trainer import Trainer
+from src.utils.callback_utils import init_improvement
+
+
+class EarlyStopping(Callback):
+    """Class for Early Stopping.
+
+    Args:
+        mode (str): One of {"min", "max"}. In min mode, training will stop
+            when the quantity monitored has stopped decreasing. In "max"
+            mode it will stop when the quantity monitored has stopped increasing.
+        patience (int): Number of epochs with no improvement after which training will be stopped.
+        min_delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+    """
+
+    def __init__(
+        self, mode: str, monitor: str, patience: int = 3, min_delta: float = 1e-6
+    ) -> None:
+        super().__init__()
+        self.mode = mode
+        self.monitor = monitor
+        self.patience = patience
+        self.min_delta = min_delta
+
+    def on_trainer_start(self, trainer: Trainer) -> None:
+        self.improvement, self.best_valid_score = init_improvement(
+            mode=self.mode, min_delta=self.min_delta
+        )
+        self.patience_counter = 0
+        self.stop = False
+        trainer.stop = self.stop  # assign to trainer
+
+    def on_valid_epoch_end(self, trainer: Trainer) -> None:
+        valid_score = trainer.valid_epoch_dict.get(self.monitor)
+        if self.improvement(
+            curr_epoch_score=valid_score, curr_best_score=self.best_valid_score
+        ):
+            # update self.best_valid_score
+            self.best_valid_score = valid_score
+
+            # reset patience counter
+            self.patience_counter = 0
+        else:
+            self.patience_counter += 1
+            print(
+                f"Early Stopping Counter {self.patience_counter} out of {self.patience}"
+            )
+
+            if self.patience_counter >= self.patience:
+                self.stop = True
+                trainer.stop = self.stop
+                print("Early Stopping!")
+
+    def should_stop(self):
+        """The actual algorithm of early stopping.
+        Consider shifting the stop logic here.
+        """
