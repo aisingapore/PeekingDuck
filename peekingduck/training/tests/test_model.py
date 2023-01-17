1b41d162ac0722f4347aa71ab456a4a856fd4032
frankcaoyun <frankprvt@gmail.com>
2023-01-17 16:22:58 +0800

    temp: save file changes due to Frank's laptop crash

---
+++ peekingduck/training/tests/test_model.py
@@ -0,0 +1,11 @@
+# test items:
+# whether the pre-trained model can load the weights correctly by comparing the
+# loaded weights with the ground truth weights. For PyTorch it is ordereddict.
+
+
+# test the forward pass function
+# def forward_pass(self, inputs: torch.Tensor) -> torch.Tensor:
+#     """Forward pass of the model."""
+#     y = self.model(inputs)
+#     print(f"X: {inputs.shape}, Y: {y.shape}")
+#     print("Forward Pass Successful")
