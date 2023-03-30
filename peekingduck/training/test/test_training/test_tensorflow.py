b73c88cbdd51852b0bd1c575d1ececaf44068248
Teck Meng <furyx@hotmail.com>
2023-03-30 21:00:37 +0800

    test: unit test for Tensorflow datasource

--- peekingduck/training/test/test_training/test_tensorflow.py
+++ peekingduck/training/test/test_training/test_tensorflow.py
@@ -34,6 +34,7 @@ from tensorflow.keras import layers
 
 
 def test_keras_example() -> None:
+    """Test simple keras model"""
     # Model / data parameters
     num_classes = 10
     input_shape = (28, 28, 1)
@@ -47,9 +48,6 @@ def test_keras_example() -> None:
     # Make sure images have shape (28, 28, 1)
     x_train = np.expand_dims(x_train, -1)
     x_test = np.expand_dims(x_test, -1)
-    print("x_train shape:", x_train.shape)
-    print(x_train.shape[0], "train samples")
-    print(x_test.shape[0], "test samples")
 
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -72,12 +70,9 @@ def test_keras_example() -> None:
         ]
     )
 
-    model.summary()
-
     """
     ## Train the model
     """
-
     batch_size = 128
     epochs = 5
 
@@ -92,9 +87,6 @@ def test_keras_example() -> None:
     """
     ## Evaluate the trained model
     """
-
     score = model.evaluate(x_test, y_test, verbose=0)
-    print("Test loss:", score[0])
-    print("Test accuracy:", score[1])
 
     assert score[1] >= 0.98
