b4970dc14095b90d453386c7c7a748271fa3d4f3
Teck Meng <furyx@hotmail.com>
2023-04-01 14:45:20 +0800

    test: add tensorflow model test

--- peekingduck/training/test/test_training/test_trainer.py
+++ peekingduck/training/test/test_training/test_trainer.py
@@ -23,6 +23,11 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 from src.training_pipeline import init_trainer
+from src.model.tensorflow_model import TFClassificationModelFactory
+from src.losses.adapter import LossAdapter
+from src.metrics.tensorflow_metrics import TensorflowMetrics
+
+# from src.callbacks.tensorflow_callbacks import TensorFlowCallbacksAdapter
 
 
 @mark.parametrize(
@@ -42,7 +47,128 @@ from src.training_pipeline import init_trainer
                 "data_module.data_adapter.tensorflow.validation.batch_size=128",
                 "data_module.data_adapter.tensorflow.test.batch_size=128",
                 "model.tensorflow.activation=null",
-                # "model.tensorflow.model_name=ConvNeXtTiny",
+                "model.tensorflow.model_name=ResNet50",
+                "model.tensorflow.fine_tune=False",
+                "trainer.tensorflow.loss_params.loss_func=SparseCategoricalCrossentropy",
+                "trainer.tensorflow.loss_params.loss_params.from_logits=True",
+            ],
+            "categorical_accuracy",
+            0.5,
+        ),
+    ],
+)
+def test_tensorflow_model(
+    overrides: List[str], validation_loss_key: str, expected: float
+) -> None:
+    """Test data_module"""
+    with initialize(version_base=None, config_path="../../configs"):
+        cfg = compose(
+            config_name="config",
+            overrides=overrides,
+        )
+
+        # """Test Tensorflow datasource"""
+        # Step 1: Create your input pipeline
+        # ### Load a dataset
+        (ds_train, ds_test), ds_info = tfds.load(
+            "mnist",
+            split=["train[:400]", "test[:40]"],
+            shuffle_files=True,
+            as_supervised=True,
+            with_info=True,
+        )
+
+        # ### Build a training pipeline
+        def normalize_img(image: tf.Tensor, label: tf.Tensor) -> Tuple[Any, Any]:
+            """Normalizes images: `uint8` -> `float32`."""
+            image = tf.cast(image, tf.float32)
+            image = image * 1 / 255.0
+            image = tf.image.resize(image, (224, 224))
+            image = tf.image.grayscale_to_rgb(image)
+            return image, label
+
+        ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+        ds_train = ds_train.cache()
+        ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+        ds_train = ds_train.batch(128)
+        ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+        # ### Build an evaluation pipeline
+        ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+        ds_test = ds_test.batch(128)
+        ds_test = ds_test.cache()
+        ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+        model_config = cfg.model[cfg.framework]
+        metrics_config = cfg.metrics[cfg.framework]
+        # callbacks_config = cfg.callbacks[cfg.framework]
+        trainer_config = cfg.trainer[cfg.framework]
+        # data_config = cfg.data_module
+        train_params = trainer_config.global_train_params
+        epochs = train_params.epochs
+
+        # Set Seed
+        tf.random.set_seed(train_params.manual_seed)
+
+        # create model
+        model = TFClassificationModelFactory.create_model(model_config)
+        # init_optimizer
+        # opt = OptimizersAdapter.get_tensorflow_optimizer(
+        #     trainer_config.optimizer_params.optimizer,
+        #     scheduler,
+        #     trainer_config.optimizer_params.optimizer_params,
+        # )
+
+        # loss
+        loss = LossAdapter.get_tensorflow_loss_func(
+            trainer_config.loss_params.loss_func,
+            trainer_config.loss_params.loss_params,
+        )
+
+        # metric
+        metrics = TensorflowMetrics().get_metrics(metrics=metrics_config)
+
+        # compile model
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(0.001), loss=loss, metrics=metrics
+        )
+
+        history = model.fit(
+            ds_train,
+            epochs=epochs,
+            validation_data=ds_test,
+            verbose="0",
+        )
+
+        # dataset = ds_train.take(5)
+        # print(list(dataset.as_numpy_iterator()))
+
+        assert history is not None
+        assert hasattr(history, "history")
+        assert validation_loss_key in history.history
+        assert len(history.history[validation_loss_key]) != 0
+        assert history.history[validation_loss_key][-1] >= expected
+
+
+@mark.skip(reason="debug tensorflow test")
+@mark.parametrize(
+    "overrides, validation_loss_key, expected",
+    [
+        (
+            [
+                "project_name=cifar10",
+                "data_module=cifar10",
+                "framework=tensorflow",
+                "debug=True",
+                "device=cpu",
+                "data_module.dataset.download=False",
+                "data_module.dataset.image_size=224",
+                "data_module.dataset.num_classes=1",
+                "data_module.data_adapter.tensorflow.train.batch_size=128",
+                "data_module.data_adapter.tensorflow.validation.batch_size=128",
+                "data_module.data_adapter.tensorflow.test.batch_size=128",
+                "model.tensorflow.activation=null",
+                "model.tensorflow.model_name=ResNet50",
                 "model.tensorflow.fine_tune=False",
                 "trainer.tensorflow.loss_params.loss_func=SparseCategoricalCrossentropy",
                 "trainer.tensorflow.loss_params.loss_params.from_logits=True",
@@ -80,8 +206,6 @@ def test_tensorflow_trainer(
             image = image * 1 / 255.0
             image = tf.image.resize(image, (224, 224))
             image = tf.image.grayscale_to_rgb(image)
-            print(image)
-            print(label)
             return image, label
 
         ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
